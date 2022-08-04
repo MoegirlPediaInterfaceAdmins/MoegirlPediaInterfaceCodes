a7f6fabeaeff711b7a7dc59b4db5ae04bffecf6e
AnnAngela <naganjue@vip.qq.com>
2022-08-04 08:53:45 +0800

    refactor(Wikiplus): use `for` loop instead of `setInterval`

--- src/gadgets/Wikiplus/MediaWiki:Gadget-Wikiplus.js
+++ src/gadgets/Wikiplus/MediaWiki:Gadget-Wikiplus.js
@@ -7,29 +7,33 @@
         cache: true,
     });
     console.info("Wikiplus load done.");
-    let i = 0;
-    const c = setInterval(() => {
+    const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+    for (let i = 0; i < Number.MAX_SAFE_INTEGER; i++) {
+        await sleep(100);
         if (!window.Wikiplus) {
             console.info("Wikiplus init running.");
-        } else if (window.Wikiplus.kotori && window.Wikiplus.kotori.inited) {
-            clearInterval(c);
-            console.info("Wikiplus init done.");
-            setTimeout(() => {
-                if (!document.querySelector("#Wikiplus-Edit-TopBtn")) {
-                    console.info("Wikiplus init incomplete.");
-                    window.Wikiplus.initQuickEdit(); //加载快速编辑
-                    if (!window.Wikiplus.getSetting("disableEditEveryWhere")) {
-                        window.Wikiplus.editEveryWhere(); //任意编辑
-                    }
-                }
-            }, 1000);
-        } else if (window.Wikiplus.kotori && i++ % 20 === 0) {
-            console.info("Wikiplus init still.");
-        } else if (window.Wikiplus.inValidNameSpaces.includes(mw.config.get("wgNamespaceNumber")) || !mw.config.get("wgIsArticle") || mw.config.get("wgAction") !== "view" || !mw.config.get("wgIsProbablyEditable") || !mw.config.get("wgUserGroups").includes("autoconfirmed") && !mw.config.get("wgUserGroups").includes("confirmed")) {
-            clearInterval(c);
-            console.info("Wikiplus init exit.");
-        } else {
-            console.info("Wikiplus init await.");
+            continue;
         }
-    }, 100);
+        if (window.Wikiplus.kotori && window.Wikiplus.kotori.inited) {
+            console.info("Wikiplus init done.");
+            await sleep(1000);
+            if (!document.querySelector("#Wikiplus-Edit-TopBtn")) {
+                console.info("Wikiplus init incomplete.");
+                window.Wikiplus.initQuickEdit(); //加载快速编辑
+                if (!window.Wikiplus.getSetting("disableEditEveryWhere")) {
+                    window.Wikiplus.editEveryWhere(); //任意编辑
+                }
+            }
+            return;
+        }
+        if (window.Wikiplus.kotori && i++ % 20 === 0) {
+            console.info("Wikiplus init still.");
+            continue;
+        }
+        if (window.Wikiplus.inValidNameSpaces.includes(mw.config.get("wgNamespaceNumber")) || !mw.config.get("wgIsArticle") || mw.config.get("wgAction") !== "view" || !mw.config.get("wgIsProbablyEditable") || !mw.config.get("wgUserGroups").includes("autoconfirmed") && !mw.config.get("wgUserGroups").includes("confirmed")) {
+            console.info("Wikiplus init exit.");
+            return;
+        }
+        console.info("Wikiplus init await.");
+    }
 })();
