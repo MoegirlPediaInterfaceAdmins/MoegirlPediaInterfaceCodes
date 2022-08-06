3f2429d611fcaa8d5badb4d548e2eeaca4aec858
Frank <25704248+leranjun@users.noreply.github.com>
2022-08-07 00:37:24 +0800

    fix(usergroup): add detailedBlockInfo class

--- src/gadgets/usergroup/MediaWiki:Gadget-usergroup.js
+++ src/gadgets/usergroup/MediaWiki:Gadget-usergroup.js
@@ -280,7 +280,7 @@ $(() => (async () => {
                     self.addClass("markBlockInfo").removeClass("unknownBlockInfo");
                     if (blockInfo.isBlocked) {
                         self.css("text-decoration", "underline wavy");
-                        self.after(`<sup title="${blockInfo.info}">[封+]</sup>`);
+                        self.after(`<sup class="detailedBlockInfo" title="${blockInfo.info}">[封+]</sup>`);
                     }
                 }
             });
