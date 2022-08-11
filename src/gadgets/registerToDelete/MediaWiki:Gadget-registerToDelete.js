9da330cba0de5bb05f303fbf7e86b2dc31f3df94
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2022-08-11 15:48:25 +0800

    fix(registerToDelete): 修复特殊页面加载挂删工具的问题

--- src/gadgets/registerToDelete/MediaWiki:Gadget-registerToDelete.js
+++ src/gadgets/registerToDelete/MediaWiki:Gadget-registerToDelete.js
@@ -2,7 +2,7 @@
 "use strict";
 $(() => {
     try {
-        if ($(".noarticletext")[0] || $(".will2Be2Deleted")[0] || !mw.config.get("wgUserGroups").includes("patroller") && !(new URL(location.href).searchParams.get("AnnTools-debug") || "").split("|").includes("registerToDelete")) {
+        if ($(".noarticletext")[0] || $(".will2Be2Deleted")[0] || mw.config.get("wgRevisionId") === 0 || !mw.config.get("wgUserGroups").includes("patroller") && !(new URL(location.href).searchParams.get("AnnTools-debug") || "").split("|").includes("registerToDelete")) {
             return;
         }
 
