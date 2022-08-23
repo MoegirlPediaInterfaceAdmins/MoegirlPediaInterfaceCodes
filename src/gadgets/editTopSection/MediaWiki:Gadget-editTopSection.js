69c44e05e0bd57499cca0c34adc7b458e0fb04fa
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2022-08-23 10:06:34 +0800

    chore(editTopSection): 不需要转换title

--- src/gadgets/editTopSection/MediaWiki:Gadget-editTopSection.js
+++ src/gadgets/editTopSection/MediaWiki:Gadget-editTopSection.js
@@ -2,7 +2,7 @@
 // <pre>
 $(() => {
     if ($(".mw-editsection")[0] && !$("#template-documentation, .template-documentation")[0]) {
-        $("#ca-edit").after(`<li id="ca-editTopSection"><span class="mw-editsection"><a href="${$("#ca-edit a").attr("href")}&section=0" title="${wgULS("编辑本页序言", "編輯本頁序言")}">${wgULS("编辑序言", "編輯序言")}</a><span class="mw-editsection-bracket"></span></span></li>`);
+        $("#ca-edit").after(`<li id="ca-editTopSection"><span class="mw-editsection"><a href="${$("#ca-edit a").attr("href")}&section=0" title="编辑本页序言">${wgULS("编辑序言", "編輯序言")}</a><span class="mw-editsection-bracket"></span></span></li>`);
     }
 });
 // </pre>
