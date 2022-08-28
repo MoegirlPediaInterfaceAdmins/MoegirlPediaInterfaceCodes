adbb5a61c54d87be82e4d8c8ce173668378e56c2
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2022-08-28 14:14:39 +0800

    feat(group-staff):默认不勾选「通过Special:最近更改和监视列表通知这些编辑」

--- src/groups/MediaWiki:Group-staff.js
+++ src/groups/MediaWiki:Group-staff.js
@@ -9,4 +9,8 @@ $(() => {
             return ele.value === "1 day";
         })[0].before(new Option("30分钟", "30 minutes"), new Option("2小时", "2 hours"), new Option("6小时", "6 hours"));
     }
+    //替换文本默认不勾选「通过Special:最近更改和监视列表通知这些编辑」
+    if (mw.config.get("wgCanonicalSpecialPageName") === "ReplaceText" && $("#doAnnounce")[0]) {
+        $("#doAnnounce").prop("checked", false);
+    }
 });
