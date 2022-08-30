3242aa9088e12439abad6d06babe3d4802e505a5
AnnAngela <naganjue@vip.qq.com>
2022-08-30 13:06:30 +0000

    chore: Log

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 if [[ "$CODESPACES" == "true" ]]; then
+    echo "Running in GitHub Codespaces, preparing nodejs & npm ..."
     wget https://raw.githubusercontent.com/tj/n/master/bin/n -O /tmp/n 
     sudo N_PRESERVE_NPM=0 bash /tmp/n lts 
     rm /tmp/n
-    sudo npm install n eslint --location=global 
-    sudo npm up --location=global 
+    sudo npm install n eslint -g
+    sudo npm up -g
     echo \"export N_PRESERVE_NPM=1\" | sudo tee -a /root/.bashrc
+    echo "Nodejs & npm installed."
 fi
 npm install
