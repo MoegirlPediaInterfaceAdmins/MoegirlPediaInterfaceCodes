75734dc2073a5714d0444da04016b88097e7b0b3
AnnAngela <naganjue@vip.qq.com>
2022-08-30 21:01:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 if [[ "$CODESPACES" == "true" ]]; then
     wget https://raw.githubusercontent.com/tj/n/master/bin/n -O /tmp/n 
-    N_PRESERVE_NPM=0 bash /tmp/n lts 
+    sudo N_PRESERVE_NPM=0 bash /tmp/n lts 
     rm /tmp/n
-    npm install n eslint --location=global 
-    npm up --location=global 
+    sudo npm install n eslint --location=global 
+    sudo npm up --location=global 
     echo \"export N_PRESERVE_NPM=1\" | sudo tee -a /root/.bashrc
 fi
+npm install
