ea431c59a38bafdffa7d11861febe10178f3b401
AnnAngela <naganjue@vip.qq.com>
2022-08-31 12:41:42 +0000

    refactor(Gadget-jQueryLazyload): Change the widget to manually maintain

--- src/gadgets/jQueryLazyload/MediaWiki:Gadget-jQueryLazyload.js
+++ src/gadgets/jQueryLazyload/MediaWiki:Gadget-jQueryLazyload.js
@@ -12,9 +12,10 @@
  * Version: 2.0.0-rc.2
  *
  */
+
 "use strict";
-(function () {
-    var defaults = {
+(() => {
+    const defaults = {
         src: "data-src",
         srcset: "data-srcset",
         selector: ".lazyload",
@@ -22,6 +23,7 @@
         rootMargin: "0px",
         threshold: 0,
     };
+
     /**
     * Merge two or more objects. Returns a new object.
     * @private
@@ -29,66 +31,62 @@
     * @param {Object}   objects  The objects to merge together
     * @returns {Object}          Merged values of defaults and options
     */
-    var extend = function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        var extended = {};
-        var deep = false;
-        var i = 0;
-        var length = args.length;
-        /* Check if a deep merge */
-        if (Object.prototype.toString.call(args[0]) === "[object Boolean]") {
-            deep = args[0];
-            i++;
-        }
+    const extend = function (_deep, ..._args) {
+
+        const extended = {};
+        const deep = typeof _deep === "boolean" ? _deep : false;
+        const args = [...typeof _deep === "boolean" ? [_deep] : [], ..._args];
+
         /* Merge the object into the extended object */
-        var merge = function (obj) {
-            for (var prop in obj) {
+        const merge = function (obj) {
+            for (const prop in obj) {
                 if (Object.prototype.hasOwnProperty.call(obj, prop)) {
                     /* If deep merge and property is an object, merge properties */
                     if (deep && Object.prototype.toString.call(obj[prop]) === "[object Object]") {
                         extended[prop] = extend(true, extended[prop], obj[prop]);
-                    }
-                    else {
+                    } else {
                         extended[prop] = obj[prop];
                     }
                 }
             }
         };
+
         /* Loop through each object and conduct a merge */
-        for (; i < length; i++) {
-            var obj = args[i];
+        for (const obj of args) {
             merge(obj);
         }
+
         return extended;
     };
-    var LazyLoad = /** @class */ (function () {
-        function LazyLoad(images, options) {
+
+    class LazyLoad {
+        constructor(images, options) {
             this.settings = extend(defaults, options || {});
             this.images = images || document.querySelectorAll(this.settings.selector);
             this.observer = null;
             this.init();
         }
-        LazyLoad.prototype.init = function () {
+        init() {
+
             /* Without observers load everything and bail out early. */
             if (!window.IntersectionObserver) {
                 this.loadImages();
                 return;
             }
-            var self = this;
-            var observerConfig = {
+
+            const self = this;
+            const observerConfig = {
                 root: this.settings.root,
                 rootMargin: this.settings.rootMargin,
                 threshold: [this.settings.threshold],
             };
-            this.observer = new IntersectionObserver(function (entries) {
-                Array.prototype.forEach.call(entries, function (entry) {
+
+            this.observer = new IntersectionObserver((entries) => {
+                Array.prototype.forEach.call(entries, (entry) => {
                     if (entry.isIntersecting) {
                         self.observer.unobserve(entry.target);
-                        var src = entry.target.getAttribute(self.settings.src);
-                        var srcset = entry.target.getAttribute(self.settings.srcset);
+                        const src = entry.target.getAttribute(self.settings.src);
+                        const srcset = entry.target.getAttribute(self.settings.srcset);
                         if ("img" === entry.target.tagName.toLowerCase()) {
                             if (src) {
                                 entry.target.src = src;
@@ -96,32 +94,33 @@
                             if (srcset) {
                                 entry.target.srcset = srcset;
                             }
-                        }
-                        else {
-                            entry.target.style.backgroundImage = "url(" + src + ")";
+                        } else {
+                            entry.target.style.backgroundImage = `url(${src})`;
                         }
                     }
                 });
             }, observerConfig);
-            Array.prototype.forEach.call(this.images, function (image) {
+
+            Array.prototype.forEach.call(this.images, (image) => {
                 self.observer.observe(image);
             });
-        };
-        LazyLoad.prototype.loadAndDestroy = function () {
+        }
+        loadAndDestroy() {
             if (!this.settings) {
                 return;
             }
             this.loadImages();
             this.destroy();
-        };
-        LazyLoad.prototype.loadImages = function () {
+        }
+        loadImages() {
             if (!this.settings) {
                 return;
             }
-            var self = this;
-            Array.prototype.forEach.call(this.images, function (image) {
-                var src = image.getAttribute(self.settings.src);
-                var srcset = image.getAttribute(self.settings.srcset);
+
+            const self = this;
+            Array.prototype.forEach.call(this.images, (image) => {
+                const src = image.getAttribute(self.settings.src);
+                const srcset = image.getAttribute(self.settings.srcset);
                 if ("img" === image.tagName.toLowerCase()) {
                     if (src) {
                         image.src = src;
@@ -129,30 +128,30 @@
                     if (srcset) {
                         image.srcset = srcset;
                     }
-                }
-                else {
-                    image.style.backgroundImage = "url('" + src + "')";
+                } else {
+                    image.style.backgroundImage = `url('${src}')`;
                 }
             });
-        };
-        LazyLoad.prototype.destroy = function () {
+        }
+        destroy() {
             if (!this.settings) {
                 return;
             }
             this.observer.disconnect();
             this.settings = null;
-        };
-        return LazyLoad;
-    }());
+        }
+    }
+
+
     window.lazyload = function (images, options) {
         return new LazyLoad(images, options);
     };
+
     if (window.jQuery) {
-        var $_1 = window.jQuery;
-        $_1.fn.lazyload = function (_options) {
-            var options = _options || {};
+        jQuery.fn.lazyload = function (_options) {
+            const options = _options || {};
             options.attribute = options.attribute || "data-src";
-            new LazyLoad($_1.makeArray(this), options);
+            new LazyLoad(this.toArray(), options);
             return this;
         };
     }
