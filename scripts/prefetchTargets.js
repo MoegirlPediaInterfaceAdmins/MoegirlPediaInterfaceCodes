7f90de2c9d0753014e697a57371f71e58c3dd525
AnnAngela <naganjue@vip.qq.com>
2022-08-31 13:34:11 +0000

    feat(Gadget-InPageEdit-v2): Use prefetch to load

--- scripts/prefetchTargets.js
+++ scripts/prefetchTargets.js
@@ -8,4 +8,9 @@ module.exports = [
         url: "https://cdn.jsdelivr.net/npm/wikiplus-highlight",
         file: "src/gadgets/wikiplus-highlight/MediaWiki:Gadget-wikiplus-highlight.js",
     },
+    {
+        name: "InPageEdit-v2",
+        url: "https://fastly.jsdelivr.net/npm/mediawiki-inpageedit",
+        file: "src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2.js",
+    },
 ];
---
+++ src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2-hook.js
@@ -0,0 +1,27 @@
+"use strict";
+if (mw.config.get("skin") === "vector") {
+    mw.hook("InPageEdit").add((ctx) => {
+        const InPageEdit = ctx.InPageEdit,
+            _msg = ctx._msg,
+            wgPageName = mw.config.get("wgRelevantPageName"),
+            wgRevisionId = mw.config.get("wgRevisionId");
+        $("#ca-edit").after(
+            $("<li>", {
+                id: "ca-quick-edit",
+                "class": "collapsible",
+            }).append(
+                $("<span>").append(
+                    $("<a>", {
+                        href: "javascript:void(0)",
+                        text: typeof Wikiplus !== "undefined" ? `${_msg("quick-edit")}(IPE)` : _msg("quick-edit"),
+                    }).on("click", () => {
+                        InPageEdit.quickEdit({
+                            page: wgPageName,
+                            revision: wgRevisionId || undefined,
+                        });
+                    }),
+                ),
+            ),
+        );
+    });
+}
