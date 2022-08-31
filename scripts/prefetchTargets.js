624fb9d7cc17042688a7dae45a4f07500bc34468
AnnAngela <naganjue@vip.qq.com>
2022-08-31 12:43:27 +0000

    ci: Add prefetch CI

---
+++ scripts/prefetchTargets.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * @type {{ url: string, file: string}[]}
+ */
+module.exports = [
+    {
+        name: "wikiplus-highlight",
+        url: "https://cdn.jsdelivr.net/npm/wikiplus-highlight",
+        file: "src/gadgets/wikiplus-highlight/MediaWiki:Gadget-wikiplus-highlight.js",
+    },
+];
