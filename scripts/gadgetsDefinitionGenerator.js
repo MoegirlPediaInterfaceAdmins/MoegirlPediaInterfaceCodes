fb065032ba21a019c46e0b7ed2b645f3cd5bff01
AnnAngela <naganjue@vip.qq.com>
2022-09-01 15:42:07 +0000

    ci: Rename

--- scripts/gadgetsDefinitionGenerator.js
+++ scripts/gadgetsDefinitionGenerator.js
@@ -9,8 +9,8 @@ const gadgetBaseRoot = "src/gadgets";
 /**
  * @type { { name: string, gadgets: string[] }[] }
  */
-const gadgetsDefinition = JSON.parse(fs.readFileSync(path.join(gadgetBaseRoot, "Gadgets-definition-list.json"), "utf-8"));
-console.info("gadgetsDefinition:", gadgetsDefinition);
+const gadgetsDefinitionList = JSON.parse(fs.readFileSync(path.join(gadgetBaseRoot, "Gadgets-definition-list.json"), "utf-8"));
+console.info("gadgetsDefinitionList:", gadgetsDefinitionList);
 for (const gadgetDirent of fs.readdirSync(gadgetBaseRoot, { withFileTypes: true })) {
     if (!gadgetDirent.isDirectory()) {
         continue;
@@ -24,7 +24,7 @@ for (const gadgetDirent of fs.readdirSync(gadgetBaseRoot, { withFileTypes: true 
         const { section } = JSON.parse(fs.readFileSync(path.join(gadgetBaseRoot, gadget, "definition.json"), "utf-8"));
         console.info(`[${gadget}]`, "section:", section);
         let sectionExist = false;
-        for (const { name, gadgets } of gadgetsDefinition) {
+        for (const { name, gadgets } of gadgetsDefinitionList) {
             if (section === name) {
                 sectionExist = true;
                 if (!gadgets.includes(gadget)) {
@@ -42,13 +42,13 @@ for (const gadgetDirent of fs.readdirSync(gadgetBaseRoot, { withFileTypes: true 
         }
         if (!sectionExist) {
             console.info(`[${gadget}]`, "section not existed, push:", { name: section, gadgets: [gadget] });
-            gadgetsDefinition.push({ name: section, gadgets: [gadget] });
+            gadgetsDefinitionList.push({ name: section, gadgets: [gadget] });
         }
     } catch (error) {
         console.error(`[${gadget}]`, "error:", error);
         process.exit(1);
     }
 }
-console.info("gadgetsDefinition final:", gadgetsDefinition);
-fs.writeFileSync(path.join(gadgetBaseRoot, "Gadgets-definition-list.json"), JSON.stringify(gadgetsDefinition, null, 4));
+console.info("gadgetsDefinitionList final:", gadgetsDefinitionList);
+fs.writeFileSync(path.join(gadgetBaseRoot, "Gadgets-definition-list.json"), JSON.stringify(gadgetsDefinitionList, null, 4));
 process.exit(0);
