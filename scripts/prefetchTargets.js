a756349e57fc402e56efdc737940f6526ad2a811
AnnAngela <naganjue@vip.qq.com>
2022-09-01 03:21:53 +0000

    ci: Introduce stylelint

--- scripts/prefetchTargets.js
+++ scripts/prefetchTargets.js
@@ -13,4 +13,14 @@ module.exports = [
         url: "https://fastly.jsdelivr.net/npm/mediawiki-inpageedit",
         file: "src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2.js",
     },
+    {
+        name: "jquery.ui-js",
+        url: "https://zh.wikipedia.org/w/load.php?debug=true&lang=zh-cn&modules=jquery.ui&skin=vector&only=scripts",
+        file: "src/gadgets/jquery.ui/MediaWiki:Gadget-jquery.ui.js",
+    },
+    {
+        name: "jquery.ui-css",
+        url: "https://zh.wikipedia.org/w/load.php?debug=true&lang=zh-cn&modules=jquery.ui&skin=vector&only=styles",
+        file: "src/gadgets/jquery.ui/MediaWiki:Gadget-jquery.ui.css",
+    },
 ];
