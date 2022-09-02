0c78e388d7ce6da2e5a8f1a2e08c93c70fadb5bf
AnnAngela <naganjue@vip.qq.com>
2022-09-02 09:27:05 +0000

    revert(Gadget-InPageEdit-v2): Revert to original solution

--- scripts/prefetchTargets.js
+++ scripts/prefetchTargets.js
@@ -8,11 +8,11 @@ module.exports = [
         url: "https://cdn.jsdelivr.net/npm/wikiplus-highlight",
         file: "src/gadgets/wikiplus-highlight/MediaWiki:Gadget-wikiplus-highlight.js",
     },
-    {
+    /* {
         name: "InPageEdit-v2",
         url: "https://cdn.jsdelivr.net/npm/mediawiki-inpageedit",
         file: "src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2.js",
-    },
+    }, */
     {
         name: "jquery.ui-js",
         url: "https://zh.wikipedia.org/w/load.php?debug=true&lang=zh-cn&modules=jquery.ui&skin=vector&only=scripts",
