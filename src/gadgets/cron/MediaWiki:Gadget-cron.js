f3cb7fee5f8ae0b24e4facb7f19c021ec654e0a2
AnnAngela <AnnAngela@users.noreply.github.com>
2022-09-03 03:32:46 +0000

    auto: auto browserify - commit from GitHub Actions

--- src/gadgets/cron/MediaWiki:Gadget-cron.js
+++ src/gadgets/cron/MediaWiki:Gadget-cron.js
@@ -1,3 +1,4 @@
+const require = () => global.luxon;
 (function(){
 var createModuleFactory = function createModuleFactory(t){var e;return function(r){return e||t(e={exports:{},parent:r},e.exports),e.exports}};
 var _$CronJob_3 = createModuleFactory(function (module, exports) {
