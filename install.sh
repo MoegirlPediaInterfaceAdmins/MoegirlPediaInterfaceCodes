e11abda9cbbce4e5da8ae035994e18a53cdc35eb
AnnAngela <naganjue@vip.qq.com>
2022-09-16 01:44:03 +0000

    chore: Fix env error

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@ if [[ "$CODESPACES" == "true" ]]; then
     echo "Running in GitHub Codespaces, preparing nodejs & npm ..."
     rm -rf /opt/nodejs ~/.nvs ~/.nvm ~/.nodejs
     wget https://raw.githubusercontent.com/tj/n/master/bin/n -O /tmp/n 
-    sudo N_PRESERVE_NPM=0 bash /tmp/n lts 
+    N_PRESERVE_NPM=0 sudo bash /tmp/n lts 
     rm /tmp/n
-    sudo npm install npm n eslint -g
-    sudo npm up -g
+    sudo /usr/local/bin/npm install npm n eslint -g
+    sudo /usr/local/bin/npm up -g
     echo "export N_PRESERVE_NPM=1" | tee -a ~/.bashrc
     hash -r
     echo "Nodejs & npm installed."
