598768d26d97dc0414d8e4e4543440503f6bb556
AnnAngela <naganjue@vip.qq.com>
2022-10-06 07:09:48 +0000

    ci: Workaround for axios@1
    Ref: axios/axios#4999

--- scripts/modules/axios.js
+++ scripts/modules/axios.js
@@ -7,8 +7,20 @@ const http = require("http");
 const https = require("https");
 const httpAgent = new http.Agent({ keepAlive: true });
 const httpsAgent = new https.Agent({ keepAlive: true });
-module.exports = axios.create({
+const instance = axios.create({
     httpAgent,
     httpsAgent,
     decompress: true,
 });
+instance.interceptors.request.use((req) => {
+    const url = new URL(axios.getUri(req)); // getUri will build the final URI, merging defaults
+    const params = new URLSearchParams(url.searchParams);
+
+    url.search = "";
+
+    req.url = url.href; // The URL without query params
+    req.params = params; // The merged query params from defaults + request
+
+    return req;
+});
+module.exports = instance;
