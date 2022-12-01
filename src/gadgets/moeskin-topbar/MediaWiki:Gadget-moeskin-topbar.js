373be96b759ef9bfc7965a3d963992cf18a25755
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2022-12-02 00:11:17 +0800

    feat: 调整文本

--- src/gadgets/moeskin-topbar/MediaWiki:Gadget-moeskin-topbar.js
+++ src/gadgets/moeskin-topbar/MediaWiki:Gadget-moeskin-topbar.js
@@ -4,15 +4,15 @@ $(() => {
         .replace(
             /<li>萌娘百科_talk:(讨论|討論)版\|(讨论|討論)版<\/li>/,
             `<li>|${wgULS("讨论版", "討論版")}\n` +
-            `<ul><li>萌娘百科_talk:讨论版|${wgULS("讨论版", "討論版")}</li>\n` +
+            `<ul><li>萌娘百科_talk:讨论版|${wgULS("目录", "目録")}</li>\n` +
             `<li>萌娘百科_talk:讨论版/技术实现|${wgULS("技术实现", "技術實現")}</li>\n` +
             `<li>萌娘百科_talk:讨论版/权限变更|${wgULS("权限变更", "權限變更")}</li>\n` +
             `<li>萌娘百科_talk:讨论版/操作申请|${wgULS("操作申请", "操作申請")}</li>\n` +
             `<li>萌娘百科_talk:讨论版/方针政策|${wgULS("方针政策", "方針政策")}</li>\n` +
             `<li>萌娘百科_talk:讨论版/页面相关|${wgULS("页面相关", "頁面相關")}</li>\n` +
             `<li>萌娘百科_talk:讨论版/提问求助|${wgULS("提问求助", "提問求助")}</li>\n` +
-            `<li>萌娘百科_talk:讨论版/群组信息|${wgULS("群组信息", "群組資訊")}</li></ul>` +
-            "</li>");
+            `<li>萌娘百科_talk:讨论版/群组信息|${wgULS("群组信息", "群組資訊")}</li></ul></li>`
+            );
     if (+mw.user.options.get("gadget-moeskin-topbar-patrolplus", 0) === 1) {
         moetopbarplus = moetopbarplus
             .replace(
