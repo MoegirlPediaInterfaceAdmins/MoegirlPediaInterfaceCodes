4b1de2510314971dbb478c813a2e0000189d2dd2
fallenice2022 <111823793+fallenice2022@users.noreply.github.com>
2022-12-13 19:55:15 +0800

    fix: showavatar styles and upload title (#71)

--- src/gadgets/ShowAvatar/MediaWiki:Gadget-ShowAvatar.js
+++ src/gadgets/ShowAvatar/MediaWiki:Gadget-ShowAvatar.js
@@ -1,12 +1,15 @@
 "use strict";
 // <pre>
 $(() => {
-    const commonsUrl = new mw.Uri("https://commons.moegirl.org.cn");
+    const commonsUrl = new mw.Uri("https://commons.moegirl.org.cn/");
     commonsUrl.query.user = mw.config.get("wgPageName").replace(/^user:/i, "");
     commonsUrl.path = "/extensions/Avatar/avatar.php";
     const imgUrl = new mw.Uri(commonsUrl);
     imgUrl.query.user = mw.config.get("wgUserName");
-    const img = $("<img>").attr("src", imgUrl);
+    const img = $("<img>").attr({
+        src: imgUrl,
+        title: "上传头像",
+    });
     const link = $("<a>").attr("href", "https://commons.moegirl.org.cn/Special:UploadAvatar").append(img);
     $("#pt-userpage").before($('<li id="pt-avatar"></li>').append(link));
     if (mw.config.get("wgNamespaceNumber") === 2 && !mw.config.get("wgPageName").includes("/")) {
@@ -16,11 +19,12 @@ $(() => {
         $(".ns-2 #firstHeading").prepend($("<a/>").attr({
             href: hrefUrl,
             title: "查看头像",
-        }).prepend($("<img/>").attr("src", srcUrl).css({
+        }).prepend($("<img/>").attr({
+            src: srcUrl,
+            id: "user-rootpage-avatar",
+        }).css({
             width: "1.2em",
             height: "1.2em",
-            "border-radius": "10px",
-            padding: "4px",
         })));
     }
 });
