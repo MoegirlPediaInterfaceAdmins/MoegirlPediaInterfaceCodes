d2ef1a2464dc927f74d3a4c759668901668a3f95
星海 <69918945+lovelyCARDINAL@users.noreply.github.com>
2022-12-15 20:16:41 +0800

    feat: add linkconfirmed

--- src/global/MediaWiki:Moeskin.js
+++ src/global/MediaWiki:Moeskin.js
@@ -134,107 +134,107 @@
             usePageToolsButton,
             addPageToolsButton,
         });
-        /* 触屏链接提示 */
-        async function linkConfirm() {
-            await mw.loader.using(["mediawiki.Uri", "ext.gadget.site-lib"]);
-            if ($("body").css("position") === "static") {
-                $("body").css("position", "relative");
-            }
-            const prompt = $("<div/>").attr("class", "linkConfirmprompt");
-            prompt.css("max-width", $("#mw-content-text").width() - 4);
-            const textnode = $("<div/>").attr("class", "linkConfirmpromptTextnode");
-            const text = $("<span/>");
-            textnode.append(text);
-            const ifSupported = window.CSS && CSS.supports && CSS.supports("-webkit-line-clamp", "2");
-            if (ifSupported) {
-                text.css({
-                    display: "-webkit-box",
-                    "-webkit-line-clamp": "2",
-                    "-webkit-box-orient": "vertical",
-                    "word-break": "break-all",
-                    "text-overflow": "ellipsis",
-                    overflow: "hidden",
-                });
-            }
-            prompt.append(wgULS("您点击了一个链接，地址为：", "您點擊了一條連結，網址為："))
-                .append(textnode)
-                .append($("<div/>").attr("class", "linkConfirmpromptAlert").append(wgULS("此网页不属于本网站，不保证其安全性", "該網頁不屬於本網站，不保證其安全性")).hide());
-            prompt.append($("<a/>").text(wgULS("继续访问", "繼續前往")).on("click", () => {
-                //window.open(prompt.data('href'), '_blank', "noopener,noreferrer");
-                window.open("javascript:window.name;", `<script>location.replace("${prompt.data("href")}");/* ${Math.random()} */</script>`);
-            })).append($("<a/>").text("取消")).on("click", (e) => {
-                if (!$(e.target).is("a")) {
-                    return false;
-                }
-                prompt.fadeOut(137);
-            });
-            prompt.appendTo("body");
-            $("body").on("click", (e) => {
-                const self = $(e.target);
-                if (!self.is("#mw-content-text a, .comment-text a")) {
-                    return;
-                }
-                if (!e.target.href || /^javascript:/.test(e.target.href) || $(e.target).is(".image")) {
-                    return true;
-                }
-                if (!e.target.dataset.linkid) {
-                    e.target.dataset.linkid = uuidv4();
-                }
-                if (prompt.is(":visible") && prompt.data("linkid") === e.target.dataset.linkid) {
-                    return false;
-                }
-                prompt.fadeOut(137);
-                const uri = new mw.Uri(e.target.href);
-                if (/\.moegirl\.org(?:\.cn)?$/.test(uri.host)) {
-                    if (!self.closest(".heimu")[0] && !self.find(".heimu")[0] || self.parent().is(".reference")) {
-                        return true;
-                    }
-                    text.text(decodeURI(e.target.href));
-                    $(".linkConfirmpromptAlert").hide();
-                } else {
-                    text.text(decodeURI(e.target.href));
-                    $(".linkConfirmpromptAlert").show();
-                }
-                let promptTop = 0, promptLeft = 0;
-                let offsetParent = self;
-                do {
-                    promptTop += offsetParent.offset().top;
-                    promptLeft += offsetParent.offset().left;
-                    offsetParent = offsetParent.offsetParent();
-                } while (!offsetParent.is("html, body"));
-                promptTop += self.outerHeight() + 3;
-                promptLeft += self.outerWidth() / 2 - prompt.outerWidth() / 2;
-                if (promptTop + prompt.outerHeight() > $(document).height() - 3) {
-                    promptTop = $(document).height() - prompt.outerHeight() - 3;
-                }
-                if (promptLeft + prompt.outerWidth() > $(window).width() - 3) {
-                    promptLeft = $(window).width() - prompt.outerWidth() - 3;
-                }
-                if (promptLeft < 0) {
-                    promptLeft = 3;
-                }
-                prompt.css({
-                    top: `${promptTop}px`,
-                    left: `${promptLeft}px`,
-                });
-                if (!ifSupported) {
-                    window.setTimeout(() => {
-                        if (text.height() > 44.8) {
-                            text.text(`${text.text()}……`);
-                            while (text.height() > 44.8) {
-                                text.text(`${text.text().slice(0, -3)}……`);
-                            }
-                        }
-                    }, 0);
-                }
-                prompt.data({
-                    href: e.target.href,
-                    linkid: self[0].dataset.linkid,
-                });
-                prompt.fadeIn(137);
-                return false;
+    }
+    //链接提示
+    async function linkConfirm() {
+        await mw.loader.using(["mediawiki.Uri"]);
+        if ($("body").css("position") === "static") {
+            $("body").css("position", "relative");
+        }
+        const prompt = $("<div/>").attr("class", "linkConfirmprompt");
+        prompt.css("max-width", $("#mw-content-text").width() - 4);
+        const textnode = $("<div/>").attr("class", "linkConfirmpromptTextnode");
+        const text = $("<span/>");
+        textnode.append(text);
+        const ifSupported = window.CSS && CSS.supports && CSS.supports("-webkit-line-clamp", "2");
+        if (ifSupported) {
+            text.css({
+                display: "-webkit-box",
+                "-webkit-line-clamp": "2",
+                "-webkit-box-orient": "vertical",
+                "word-break": "break-all",
+                "text-overflow": "ellipsis",
+                overflow: "hidden",
             });
         }
+        prompt.append(wgULS("您点击了一个链接，地址为：", "您點擊了一條連結，網址為："))
+            .append(textnode)
+            .append($("<div/>").attr("class", "linkConfirmpromptAlert").append(wgULS("此网页不属于本网站，不保证其安全性", "該網頁不屬於本網站，不保證其安全性")).hide());
+        prompt.append($("<a/>").text(wgULS("继续访问", "繼續前往")).on("click", () => {
+            //window.open(prompt.data('href'), '_blank', "noopener,noreferrer");
+            window.open("javascript:window.name;", `<script>location.replace("${prompt.data("href")}");/* ${Math.random()} */</script>`);
+        })).append($("<a/>").text("取消")).on("click", (e) => {
+            if (!$(e.target).is("a")) {
+                return false;
+            }
+            prompt.fadeOut(137);
+        });
+        prompt.appendTo("body");
+        $("body").on("click", (e) => {
+            const self = $(e.target);
+            if (!self.is("#mw-content-text a, .comment-text a")) {
+                return;
+            }
+            if (!e.target.href || /^javascript:/.test(e.target.href) || $(e.target).is(".image")) {
+                return true;
+            }
+            if (!e.target.dataset.linkid) {
+                e.target.dataset.linkid = uuidv4();
+            }
+            if (prompt.is(":visible") && prompt.data("linkid") === e.target.dataset.linkid) {
+                return false;
+            }
+            prompt.fadeOut(137);
+            const uri = new mw.Uri(e.target.href);
+            if (/\.moegirl\.org(?:\.cn)?$/.test(uri.host)) {
+                if (!self.closest(".heimu")[0] && !self.find(".heimu")[0] || self.parent().is(".reference")) {
+                    return true;
+                }
+                text.text(decodeURI(e.target.href));
+                $(".linkConfirmpromptAlert").hide();
+            } else {
+                text.text(decodeURI(e.target.href));
+                $(".linkConfirmpromptAlert").show();
+            }
+            let promptTop = 0, promptLeft = 0;
+            let offsetParent = self;
+            do {
+                promptTop += offsetParent.offset().top;
+                promptLeft += offsetParent.offset().left;
+                offsetParent = offsetParent.offsetParent();
+            } while (!offsetParent.is("html, body"));
+            promptTop += self.outerHeight() + 3;
+            promptLeft += self.outerWidth() / 2 - prompt.outerWidth() / 2;
+            if (promptTop + prompt.outerHeight() > $(document).height() - 3) {
+                promptTop = $(document).height() - prompt.outerHeight() - 3;
+            }
+            if (promptLeft + prompt.outerWidth() > $(window).width() - 3) {
+                promptLeft = $(window).width() - prompt.outerWidth() - 3;
+            }
+            if (promptLeft < 0) {
+                promptLeft = 3;
+            }
+            prompt.css({
+                top: `${promptTop}px`,
+                left: `${promptLeft}px`,
+            });
+            if (!ifSupported) {
+                window.setTimeout(() => {
+                    if (text.height() > 44.8) {
+                        text.text(`${text.text()}……`);
+                        while (text.height() > 44.8) {
+                            text.text(`${text.text().slice(0, -3)}……`);
+                        }
+                    }
+                }, 0);
+            }
+            prompt.data({
+                href: e.target.href,
+                linkid: self[0].dataset.linkid,
+            });
+            prompt.fadeIn(137);
+            return false;
+        });
     }
     /* 函数执行体 */
     await $.ready;
