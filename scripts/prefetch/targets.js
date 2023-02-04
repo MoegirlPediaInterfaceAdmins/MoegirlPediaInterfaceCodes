8f9eabb86629d09ff1c35793e5e85869b668a66b
AnnAngela <naganjue@vip.qq.com>
2023-02-04 15:16:33 +0800

    ci: use unminified version

--- scripts/prefetch/targets.js
+++ scripts/prefetch/targets.js
@@ -5,18 +5,18 @@
 module.exports = [
     {
         name: "wikiplus-highlight",
-        url: "https://cdn.jsdelivr.net/npm/wikiplus-highlight",
+        url: "https://cdn.jsdelivr.net/npm/wikiplus-highlight/main.js",
         file: "src/gadgets/wikiplus-highlight/MediaWiki:Gadget-wikiplus-highlight.js",
     },
     {
         name: "luxon",
-        url: "https://cdn.jsdelivr.net/npm/luxon@3",
+        url: "https://cdn.jsdelivr.net/npm/luxon@3/build/global/luxon.js",
         file: "src/gadgets/luxon/MediaWiki:Gadget-luxon.js",
         appendCode: "window.luxon = luxon;",
     },
     /* {
         name: "InPageEdit-v2",
-        url: "https://cdn.jsdelivr.net/npm/mediawiki-inpageedit",
+        url: "https://cdn.jsdelivr.net/npm/mediawiki-inpageedit/dist/InPageEdit.js",
         file: "src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2.js",
     }, */
     {
@@ -31,7 +31,7 @@ module.exports = [
     },
     {
         name: "libJSON5",
-        url: "https://cdn.jsdelivr.net/npm/json5@2",
+        url: "https://cdn.jsdelivr.net/npm/json5@2/dist/index.js",
         file: "src/gadgets/libJSON5/MediaWiki:Gadget-libJSON5.js",
     },
 ];
