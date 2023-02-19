cd2c691ea3eeac4958805b78808d766a6b2a9145
星海 <yan@musinya.com>
2023-02-20 00:33:36 +0800

    feat: 添加繁体支持

--- src/gadgets/Logout-confirm/MediaWiki:Gadget-Logout-confirm.js
+++ src/gadgets/Logout-confirm/MediaWiki:Gadget-Logout-confirm.js
@@ -10,8 +10,8 @@
         const context = $("#mw-content-text");
         if (!context.find("#mw-returnto")[0] && context.text().includes("If you wish to log out")) {
             const link = context.find("a[href*=logoutToken]").clone();
-            link.attr("target", "_self").text("点此退出").addClass("mw-ui-button mw-ui-progressive").css("margin-left", ".25em");
-            context.empty().text("如果您想退出萌百账号，请").append(link);
+            link.attr("target", "_self").text(wgULS("点此退出", "點此登出")).addClass("mw-ui-button mw-ui-progressive").css("margin-left", ".25em");
+            context.empty().text("如果您想退出萌娘百科账号，请", "如果您想登出萌娘百科賬號，請").append(link);
         }
     }
 })();
