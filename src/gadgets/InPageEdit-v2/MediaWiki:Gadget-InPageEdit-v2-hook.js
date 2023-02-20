594d908ba2adfeb075ff0fad4c6193829d1dd618
星海 <yan@musinya.com>
2023-02-20 16:32:47 +0800

    feat: add button for moeskin

--- src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2-hook.js
+++ src/gadgets/InPageEdit-v2/MediaWiki:Gadget-InPageEdit-v2-hook.js
@@ -1,27 +1,45 @@
 "use strict";
-if (mw.config.get("skin") === "vector") {
-    mw.hook("InPageEdit").add((ctx) => {
-        const InPageEdit = ctx.InPageEdit,
-            _msg = ctx._msg,
-            wgPageName = mw.config.get("wgRelevantPageName"),
-            wgRevisionId = mw.config.get("wgRevisionId");
-        $("#ca-edit").after(
-            $("<li>", {
-                id: "ca-quick-edit",
-                "class": "collapsible",
-            }).append(
-                $("<span>").append(
-                    $("<a>", {
-                        href: "javascript:void(0)",
-                        text: typeof Wikiplus !== "undefined" ? `${_msg("quick-edit")}(IPE)` : _msg("quick-edit"),
-                    }).on("click", () => {
-                        InPageEdit.quickEdit({
-                            page: wgPageName,
-                            revision: wgRevisionId || undefined,
-                        });
-                    }),
+mw.hook("InPageEdit").add((ctx) => {
+    const InPageEdit = ctx.InPageEdit,
+        _msg = ctx._msg,
+        wgPageName = mw.config.get("wgRelevantPageName"),
+        wgRevisionId = mw.config.get("wgRevisionId");
+    switch (mw.config.get("skin")) {
+        case "vector":
+            $("#ca-edit").after(
+                $("<li>", {
+                    id: "ca-quick-edit",
+                    "class": "collapsible",
+                }).append(
+                    $("<span>").append(
+                        $("<a>", {
+                            href: "javascript:void(0)",
+                            text: typeof Wikiplus !== "undefined" ? `${_msg("quick-edit")}(IPE)` : _msg("quick-edit"),
+                        }).on("click", () => {
+                            InPageEdit.quickEdit({
+                                page: wgPageName,
+                                revision: wgRevisionId || undefined,
+                            });
+                        }),
+                    ),
                 ),
-            ),
-        );
-    });
-}
+            );
+            break;
+        case "moeskin":
+        default:
+            mw.hook("moeskin.pagetools").add(({ addPageToolsButton }) => {
+                const $btn = addPageToolsButton('<span style="align-self:center;font:0.7em bold;">IPE</span>', "快速编辑");
+                $btn.attr({
+                    id: "ca-inpageedit",
+                    href: "javascript:void(0)",
+                }).on("click", () => {
+                    InPageEdit.quickEdit({
+                        page: wgPageName,
+                        revision: wgRevisionId || undefined,
+                    });
+                });
+            });
+            $(".page-tool-link#ca-inpageedit").insertAfter($(".page-tool-link#ca-edit"));
+            break;
+    }
+});
