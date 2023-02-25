8fbc993a9451b38bd37999078bf94b0bfd9a03e2
AnnAngela <naganjue@vip.qq.com>
2023-02-25 16:47:16 +0800

    ci: Add log

--- scripts/postCommit/linguist-generated.js
+++ scripts/postCommit/linguist-generated.js
@@ -8,7 +8,9 @@ const core = require("@actions/core");
     const oldGitattributes = await fs.promises.readFile(".gitattributes", {
         encoding: "utf-8",
     });
+    console.info("old .gitattributes", oldGitattributes);
     const originalGitattributes = oldGitattributes.split("\n").filter((line) => !line.includes(" # From ")).join("\n").replace(/(?<=\n)\n+/g, "");
+    console.info("original .gitattributes", originalGitattributes);
     const newGitattributes = [""];
     for (const [index, stringValue] of Object.entries(process.env)) {
         if (!index.startsWith("linguist-generated-")) {
@@ -20,12 +22,16 @@ const core = require("@actions/core");
             core.warning(`${index} is not array or empty: ${stringValue}`);
             continue;
         }
+        console.info(key, value);
         for (const srcPath of value) {
             newGitattributes.push(`${srcPath}  linguist-generated=true # From ${key}`);
         }
         newGitattributes.push("");
     }
-    await fs.promises.writeFile(".gitattributes", [...originalGitattributes, ...newGitattributes].join("\n"), {
+    console.info("new .gitattributes", newGitattributes);
+    const finalGitattributes = [...originalGitattributes, ...newGitattributes];
+    console.info("final .gitattributes", finalGitattributes);
+    await fs.promises.writeFile(".gitattributes", finalGitattributes.join("\n"), {
         encoding: "utf-8",
     });
     process.exit(0);
