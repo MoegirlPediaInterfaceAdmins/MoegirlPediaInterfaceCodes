72569251ae80aab945e9ebcbf743381b5ccccbc7
AnnAngela <naganjue@vip.qq.com>
2023-02-25 17:48:33 +0800

    ci: Meaningless log

--- scripts/postCommit/prepareGit.js
+++ scripts/postCommit/prepareGit.js
@@ -1,21 +1,21 @@
 "use strict";
 const console = require("../modules/console.js");
 console.info("Start initialization...");
-const { git, log } = require("../modules/git.js");
+const { git } = require("../modules/git.js");
 
 (async () => {
     const name = process.env.GITHUB_ACTOR;
     const email = `${process.env.GITHUB_ACTOR}@users.noreply.github.com`;
     console.info("name:", name);
     console.info("email:", email);
-    await git.add(".");
     await git
-        .addConfig("user.email", email, undefined, log)
-        .addConfig("user.name", name, undefined, log)
-        .addConfig("author.email", email, undefined, log)
-        .addConfig("author.name", name, undefined, log)
-        .addConfig("committer.email", "GitHub Actions", undefined, log)
-        .addConfig("committer.name", "actions@github.com", undefined, log)
+        .add(".")
+        .addConfig("user.email", email)
+        .addConfig("user.name", name)
+        .addConfig("author.email", email)
+        .addConfig("author.name", name)
+        .addConfig("committer.email", "GitHub Actions")
+        .addConfig("committer.name", "actions@github.com")
         .fetch(["--tags", "--force"]);
     process.exit(0);
 })();
