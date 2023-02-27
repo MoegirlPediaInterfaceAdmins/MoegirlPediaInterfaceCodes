a73a16a03c5f2801f7f827c430ad906b820b3383
AnnAngela <naganjue@vip.qq.com>
2023-02-27 11:43:41 +0800

    ci: Fix error

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -2,12 +2,12 @@
 const { octokit, octokitBaseOptions } = require("../modules/octokit.js");
 const core = require("@actions/core");
 (async () => {
-    core.info(await octokit.rest.issues.create({
+    core.info(JSON.stringify(await octokit.rest.issues.create({
         ...octokitBaseOptions,
         title: "test",
         body: "test",
         labels: "ci:generatePolyfill",
-    }));
+    })));
     core.startGroup("process.env");
     core.info(JSON.stringify(process.env, null, 4));
     core.endGroup();
