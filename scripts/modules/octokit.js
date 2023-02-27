8439f0c495de880499e286dd3b97c5a94fcf9a55
AnnAngela <naganjue@vip.qq.com>
2023-02-27 11:40:07 +0800

    ci: octokit doesn't have a simple way to set default owner & repo =。=

--- scripts/modules/octokit.js
+++ scripts/modules/octokit.js
@@ -2,16 +2,12 @@
 const consoleWithTime = require("../modules/console.js");
 const { Octokit } = require("@octokit/rest");
 const { retry } = require("@octokit/plugin-retry");
-const octokit = new (Octokit.plugin(retry))({
-    authStrategy: require("@octokit/auth-action").createActionAuth(),
-});
 const octokitBaseOptions = {
     owner: process.env.GITHUB_REPOSITORY_OWNER,
     repo: process.env.GITHUB_REPOSITORY.split("/")[1],
 };
-octokit.hook.wrap("request", (request, options) => request({
-    ...octokitBaseOptions,
-    ...options,
-}));
+const octokit = new (Octokit.plugin(retry))({
+    authStrategy: require("@octokit/auth-action").createActionAuth(),
+});
 consoleWithTime.log("octokitBaseOptions:", octokitBaseOptions);
-module.exports = octokit;
+module.exports = { octokit, octokitBaseOptions };
--- scripts/test/test.js
+++ scripts/test/test.js
@@ -1,8 +1,9 @@
 "use strict";
-const octokit = require("../modules/octokit.js");
+const { octokit, octokitBaseOptions } = require("../modules/octokit.js");
 const core = require("@actions/core");
 (async () => {
     core.info(await octokit.rest.issues.create({
+        ...octokitBaseOptions,
         title: "test",
         body: "test",
         labels: "ci:generatePolyfill",
