61ce278233f28a5506ba73e9a4924bf2133ff662
AnnAngela <naganjue@vip.qq.com>
2023-02-27 20:52:16 +0800

    ci: Delete unnecessary logs

--- scripts/modules/octokit.js
+++ scripts/modules/octokit.js
@@ -1,5 +1,6 @@
 "use strict";
 const consoleWithTime = require("../modules/console.js");
+const core = require("@actions/core");
 const { Octokit } = require("@octokit/rest");
 const { retry } = require("@octokit/plugin-retry");
 const octokitBaseOptions = {
@@ -10,26 +11,51 @@ const octokit = new (Octokit.plugin(retry))({
     authStrategy: require("@octokit/auth-action").createActionAuth,
     auth: {},
 });
+// 非常神必，直接给 request 传入 { ...octokitBaseOptions, ...options } 没有任何作用，只能修改地址了
+octokit.hook.wrap("request", (request, options) => {
+    const url = options.url.split("/");
+    options.url = url.map((part) => part === "{owner}" ? octokitBaseOptions.owner : part === "{repo}" ? octokitBaseOptions.repo : part).join("/");
+    return request(options);
+});
 consoleWithTime.log("octokitBaseOptions:", octokitBaseOptions);
-const closeRelativeIssue = async (issueTitle, labels) => {
-    consoleWithTime.info("Searching current opened issue with labels:", labels);
+const createIssue = async (issueTitle, issueBody, labels) => {
+    consoleWithTime.info("[createIssue] Searching current opened issue with labels:", labels);
     const issues = (await octokit.rest.issues.listForRepo({
-        ...octokitBaseOptions,
-        creator: process.env.GITHUB_ACTOR,
-        labels,
-    })).data.map(({ number, title }) => ({ number, title }));
-    consoleWithTime.info("Current opened issue:", issues);
-    for (const { number, title } of issues) {
-        if (title !== issueTitle) {
-            consoleWithTime.info("issue is not relative, ignore:", { number, title });
+        labels: labels.join(","),
+    })).data;
+    core.startGroup("[createIssue] Current opened issue:");
+    consoleWithTime.info(issues);
+    core.endGroup();
+    for (const { number, title, body } of issues) {
+        console.info("[createIssue] Checking issue:", { number, title, body });
+        if (title !== issueTitle || body !== issueBody) {
+            consoleWithTime.info("[createIssue] Issue is not relative, ignore.");
             continue;
         }
-        consoleWithTime.info("issue is relative, close:", { number, title }, await octokit.rest.issues.update({
-            ...octokitBaseOptions,
+        consoleWithTime.info("[createIssue] Issue is relative, start to close issue...");
+        const result = await octokit.rest.issues.update({
             issue_number: number,
             state: "closed",
             state_reason: "Duplicated",
-        }));
+        });
+        core.startGroup("[createIssue] Successfully closed the issue:");
+        consoleWithTime.info(result);
+        core.endGroup();
     }
+    const options = {
+        title: issueTitle,
+        body: issueBody,
+        labels,
+    };
+    consoleWithTime.info("[createIssue] Start to create issue:", options);
+    const result = await octokit.rest.issues.create({
+        title: issueTitle,
+        body: issueBody,
+        labels,
+    });
+    core.startGroup("[createIssue] Successfully created the issue:");
+    consoleWithTime.info(result);
+    core.endGroup();
+    return result;
 };
-module.exports = { octokit, octokitBaseOptions, closeRelativeIssue };
+module.exports = { octokit, octokitBaseOptions, createIssue };
--- scripts/test/test.js
+++ scripts/test/test.js
@@ -1,14 +1,37 @@
 "use strict";
-const { octokit, octokitBaseOptions } = require("../modules/octokit.js");
+const { octokit } = require("../modules/octokit.js");
 const core = require("@actions/core");
 (async () => {
-    console.info(await octokit.rest.issues.create({
-        ...octokitBaseOptions,
-        title: "test",
-        body: "test",
-        labels: ["ci:test"],
-    }));
+    const lists = await octokit.rest.actions.listWorkflowRuns({
+        workflow_id: "test.yml",
+        per_page: 100,
+        created: "<2023-01-01",
+    });
+    core.startGroup("listWorkflowRuns");
+    console.info(lists);
+    core.endGroup();
+    const failed = [];
+    for (const { id } of lists) {
+        try {
+            const result = await octokit.rest.actions.deleteWorkflowRun({
+                run_id: id,
+            });
+            core.startGroup(`deleteWorkflowRun - ${id}`);
+            console.info(result);
+            core.endGroup();
+        } catch (e) {
+            core.startGroup(`[failed] deleteWorkflowRun - ${id}`);
+            console.error(e);
+            core.endGroup();
+            failed.push(id);
+        }
+    }
+    if (failed.length === 0) {
+        console.info("No failed");
+    } else {
+        console.error("failed:", failed);
+    }
     core.startGroup("process.env");
-    core.info(JSON.stringify(process.env, null, 4));
+    console.info(process.env);
     core.endGroup();
 })();
