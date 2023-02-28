26b78922ef6331f683906c8fe4b6c26b37f1b1c5
AnnAngela <naganjue@vip.qq.com>
2023-02-28 14:41:07 +0800

    ci: Use REST api to make a commit

--- scripts/modules/createCommit.js
+++ scripts/modules/createCommit.js
@@ -1,44 +1,64 @@
 "use strict";
+const fs = require("fs");
 const console = require("../modules/console.js");
 const { git } = require("../modules/git.js");
 const core = require("@actions/core");
-const { isInGithubActions } = require("../modules/octokit.js");
+const { isInGithubActions, octokit } = require("../modules/octokit.js");
 
 /**
  * 
  * @param {string} message 
- * @returns {CommitResult & { commit_long: string }}
  */
 module.exports = async (message) => {
-    if (isInGithubActions) {
-        console.info("[createCommit] Running in github actions, try to create commit.");
-        console.info("[createCommit] message:", message);
-        await git.add(".");
-        const changedFiles = (await git.diffSummary(["--cached"])).files.map(({ file }) => file);
-        if (changedFiles.length === 0) {
-            console.info("[createCommit] Working tree clean. Nothing to commit.");
-            return false;
-        }
-        try {
-            console.info("[createCommit] process.env.changedFiles:", process.env.changedFiles || "[]");
-            /**
+    if (!isInGithubActions) {
+        console.info("Not running in github actions, exit.");
+        return false;
+    }
+    console.info("[createCommit] Running in github actions, try to create commit.");
+    console.info("[createCommit] message:", message);
+    await git.add(".");
+    const changedFiles = (await git.diffSummary(["--cached"])).files.map(({ file }) => file);
+    if (changedFiles.length === 0) {
+        console.info("[createCommit] Working tree clean. Nothing to commit.");
+        return false;
+    }
+    try {
+        console.info("[createCommit] process.env.changedFiles:", process.env.changedFiles || "[]");
+        /**
              * @type {string[] | undefined}
              */
-            const changedFilesFromEnv = JSON.parse(process.env.changedFiles);
-            console.info("[createCommit] changedFilesFromEnv:", changedFilesFromEnv);
-            const conbineChangedFiles = [...new Set([...Array.isArray(changedFilesFromEnv) ? changedFilesFromEnv : [], ...changedFiles])];
-            console.info("[createCommit] conbineChangedFiles:", conbineChangedFiles);
-            core.exportVariable("changedFiles", JSON.stringify(conbineChangedFiles));
-        } catch (e) {
-            console.error("[createCommit] processing env error, discarded", e);
-            core.exportVariable("changedFiles", JSON.stringify(changedFiles));
-        }
-        const data = await git.commit(message);
-        console.info("[createCommit] commit_sha:", data.commit);
-        data.commit_long = await git.revparse(data.commit);
-        console.info("[createCommit] commit_long_sha:", data.commit_long);
-        console.info("[createCommit] Done.");
-        return data;
+        const changedFilesFromEnv = JSON.parse(process.env.changedFiles);
+        console.info("[createCommit] changedFilesFromEnv:", changedFilesFromEnv);
+        const conbineChangedFiles = [...new Set([...Array.isArray(changedFilesFromEnv) ? changedFilesFromEnv : [], ...changedFiles])];
+        console.info("[createCommit] conbineChangedFiles:", conbineChangedFiles);
+        core.exportVariable("changedFiles", JSON.stringify(conbineChangedFiles));
+    } catch (e) {
+        console.error("[createCommit] processing env error, discarded", e);
+        core.exportVariable("changedFiles", JSON.stringify(changedFiles));
     }
-    console.info("Not running in github actions, exit.");
+    let i = 1;
+    for (const file of changedFiles) {
+        console.info("Start to upload", file);
+        const result = await octokit.rest.repos.createOrUpdateFileContents({
+            path: file,
+            message: `${changedFiles.length > 1 ? `[${i++}/${changedFiles.length}] ` : ""}${message}`,
+            content: await fs.promises.readFile(file, { encoding: "base64" }),
+            sha: await git.revparse([`${process.env.GITHUB_REF}:${file}`]),
+            branch: process.env.GITHUB_REF,
+            committer: {
+                name: process.env.COMMITTER_NAME,
+                email: process.env.COMMITTER_EMAIL,
+            },
+            author: {
+                name: process.env.AUTHOR_NAME,
+                email: process.env.AUTHOR_EMAIL,
+            },
+        });
+        core.startGroup("[createCommit] Upload result:");
+        console.info(result);
+        core.endGroup();
+    }
+    await git.pull(undefined, undefined, ["--rebase"]);
+    console.info("[createCommit] Done.");
+    return true;
 };
--- scripts/postCommit/prepareGit.js
+++ scripts/postCommit/prepareGit.js
@@ -1,33 +1,26 @@
 "use strict";
 const console = require("../modules/console.js");
 console.info("Start initialization...");
-const { git } = require("../modules/git.js");
 const { isInGithubActions } = require("../modules/octokit.js");
+const core = require("@actions/core");
 
-(async () => {
-    if (isInGithubActions) {
-        console.info("Running in github actions, preparing git...");
-        const { commit, head_commit, pusher, sender } = require(process.env.GITHUB_EVENT_PATH);
-        const tmpMap = {};
-        for (const { author: { email: authorEmail, name: authorName, username: authorUsername }, committer: { email: committerEmail, name: committerName, username: committerUsername } } of [...Array.isArray(commit) ? commit : [], ...head_commit ? [head_commit] : []]) {
-            tmpMap[authorName] = tmpMap[authorUsername] = authorEmail;
-            tmpMap[committerName] = tmpMap[committerUsername] = committerEmail;
-        }
-        const name = pusher ? pusher.name : sender ? sender.login : process.env.GITHUB_ACTOR;
-        const email = pusher ? pusher.email : sender ? tmpMap[sender.login] || `${sender.login}@users.noreply.github.com` : `${process.env.GITHUB_ACTOR}@users.noreply.github.com`;
-        console.info("name:", name);
-        console.info("email:", email);
-        await git
-            .add(".")
-            .addConfig("user.email", email)
-            .addConfig("user.name", name)
-            .addConfig("author.email", email)
-            .addConfig("author.name", name)
-            .addConfig("committer.email", "actions@github.com")
-            .addConfig("committer.name", "GitHub Actions")
-            .fetch(["--tags", "--force"]);
-    } else {
-        console.info("Not running in github actions, exit.");
+if (isInGithubActions) {
+    console.info("Running in github actions, preparing git...");
+    const { commit, head_commit, pusher, sender } = require(process.env.GITHUB_EVENT_PATH);
+    const tmpMap = {};
+    for (const { author: { email: authorEmail, name: authorName, username: authorUsername }, committer: { email: committerEmail, name: committerName, username: committerUsername } } of [...Array.isArray(commit) ? commit : [], ...head_commit ? [head_commit] : []]) {
+        tmpMap[authorName] = tmpMap[authorUsername] = authorEmail;
+        tmpMap[committerName] = tmpMap[committerUsername] = committerEmail;
     }
-    process.exit(0);
-})();
+    const name = pusher ? pusher.name : sender ? sender.login : process.env.GITHUB_ACTOR;
+    const email = pusher ? pusher.email : sender ? tmpMap[sender.login] || `${sender.login}@users.noreply.github.com` : `${process.env.GITHUB_ACTOR}@users.noreply.github.com`;
+    console.info("AUTHOR_NAME:", name);
+    console.info("AUTHOR_EMAIL:", email);
+    core.exportVariable("AUTHOR_NAME", name);
+    core.exportVariable("AUTHOR_EMAIL", email);
+    core.exportVariable("COMMITTER_NAME", "GitHub Actions");
+    core.exportVariable("COMMITTER_EMAIL", "actions@github.com");
+} else {
+    console.info("Not running in github actions, exit.");
+}
+process.exit(0);
