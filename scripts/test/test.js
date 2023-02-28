ecc028802ba25e35d0de50c6bc9db65a4b8919ee
AnnAngela <naganjue@vip.qq.com>
2023-02-28 10:33:57 +0800

    ci: Output event

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -1,7 +1,11 @@
 "use strict";
+const fs = require("fs");
 const core = require("@actions/core");
-(() => {
+(async () => {
     core.startGroup("process.env");
     console.info(process.env);
+    core.endGroup();
+    core.startGroup("process.env.GITHUB_EVENT_PATH");
+    console.info(await fs.promises.readFile(process.env.GITHUB_EVENT_PATH, { encoding: "utf-8" }));
     core.endGroup();
 })();
