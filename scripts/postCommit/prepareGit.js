77d33110706158da72a078611bd22bc893e3c245
AnnAngela <naganjue@vip.qq.com>
2023-03-05 21:07:55 +0800

    ci: Not to change default git config in Actions

--- scripts/postCommit/prepareGit.js
+++ scripts/postCommit/prepareGit.js
@@ -3,37 +3,25 @@ const console = require("../modules/console.js");
 console.info("Start initialization...");
 const { isInGithubActions } = require("../modules/octokit.js");
 const core = require("@actions/core");
-const { git } = require("../modules/git.js");
 
-(async () => {
-    if (!isInGithubActions) {
-        console.info("Not running in github actions, exit.");
-        process.exit(0);
-        return;
-    }
-    console.info("Running in github actions, preparing git...");
-    const { commit, head_commit, pusher, sender } = require(process.env.GITHUB_EVENT_PATH);
-    const tmpMap = {};
-    for (const { author: { email: authorEmail, name: authorName, username: authorUsername }, committer: { email: committerEmail, name: committerName, username: committerUsername } } of [...Array.isArray(commit) ? commit : [], ...head_commit ? [head_commit] : []]) {
-        tmpMap[authorName] = tmpMap[authorUsername] = authorEmail;
-        tmpMap[committerName] = tmpMap[committerUsername] = committerEmail;
-    }
-    const name = pusher ? pusher.name : sender ? sender.login : process.env.GITHUB_ACTOR;
-    const email = pusher ? pusher.email : sender ? tmpMap[sender.login] || `${sender.login}@users.noreply.github.com` : `${process.env.GITHUB_ACTOR}@users.noreply.github.com`;
-    console.info("AUTHOR_NAME:", name);
-    console.info("AUTHOR_EMAIL:", email);
-    core.exportVariable("AUTHOR_NAME", name);
-    core.exportVariable("AUTHOR_EMAIL", email);
-    core.exportVariable("COMMITTER_NAME", "GitHub Actions");
-    core.exportVariable("COMMITTER_EMAIL", "actions@github.com");
-    await git
-        .add(".")
-        .addConfig("user.name", name)
-        .addConfig("user.email", email)
-        .addConfig("author.name", name)
-        .addConfig("author.email", email)
-        .addConfig("committer.name", "GitHub Actions")
-        .addConfig("committer.email", "actions@github.com")
-        .fetch(["--tags", "--force"]);
+
+if (!isInGithubActions) {
+    console.info("Not running in github actions, exit.");
     process.exit(0);
-})();
+}
+console.info("Running in github actions, preparing git...");
+const { commit, head_commit, pusher, sender } = require(process.env.GITHUB_EVENT_PATH);
+const tmpMap = {};
+for (const { author: { email: authorEmail, name: authorName, username: authorUsername }, committer: { email: committerEmail, name: committerName, username: committerUsername } } of [...Array.isArray(commit) ? commit : [], ...head_commit ? [head_commit] : []]) {
+    tmpMap[authorName] = tmpMap[authorUsername] = authorEmail;
+    tmpMap[committerName] = tmpMap[committerUsername] = committerEmail;
+}
+const name = pusher ? pusher.name : sender ? sender.login : process.env.GITHUB_ACTOR;
+const email = pusher ? pusher.email : sender ? tmpMap[sender.login] || `${sender.login}@users.noreply.github.com` : `${process.env.GITHUB_ACTOR}@users.noreply.github.com`;
+console.info("AUTHOR_NAME:", name);
+console.info("AUTHOR_EMAIL:", email);
+core.exportVariable("AUTHOR_NAME", name);
+core.exportVariable("AUTHOR_EMAIL", email);
+core.exportVariable("COMMITTER_NAME", "GitHub Actions");
+core.exportVariable("COMMITTER_EMAIL", "actions@github.com");
+process.exit(0);
