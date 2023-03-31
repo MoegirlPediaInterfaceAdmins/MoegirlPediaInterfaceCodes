61835f4aa10bc65e51a6bb93ed77c7f5b6b86f94
星海 <yan@musinya.com>
2023-04-01 01:33:27 +0800

    feat: 限定ns6，添加样式

--- src/gadgets/ContentAboveBelowImage/MediaWiki:Gadget-ContentAboveBelowImage.js
+++ src/gadgets/ContentAboveBelowImage/MediaWiki:Gadget-ContentAboveBelowImage.js
@@ -1,5 +1,5 @@
 // [[cm:User:星海子/js/ContentAboveBelowImage.js]]
 "use strict";
 $(() =>
-    $("#mw-imagepage-content")[0] && $(".will2Be2Deleted")[0] ? $("#file").before($("#mw-imagepage-content")) : null,
+    $(".ns-6 #mw-imagepage-content")[0] && $(".will2Be2Deleted")[0] ? $("#file").before($("#mw-imagepage-content")) : null,
 );
