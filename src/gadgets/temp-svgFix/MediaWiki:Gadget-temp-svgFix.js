bc58dbd3765cf09260ddbc0fce5cee8938b1cd81
AnnAngela <naganjue@vip.qq.com>
2023-05-15 16:14:43 +0800

    fix(Gadget-temp-svgFix): Fix lazyload miss attr

--- src/gadgets/temp-svgFix/MediaWiki:Gadget-temp-svgFix.js
+++ src/gadgets/temp-svgFix/MediaWiki:Gadget-temp-svgFix.js
@@ -2,16 +2,17 @@
 $(() => {
     // 临时修复svg缩略图损坏
     /**
-     * @type { NodeListOf<HTMLImageElement> }
+     * @type { HTMLImageElement[] }
      */
-    const svgs = document.querySelectorAll('img[src$=".svg.png"]');
+    const svgs = Array.from(document.querySelectorAll('img[src$=".svg.png"]'));
     for (const img of svgs) {
         try {
-            const url = new mw.Uri(img.src);
+            const src = img.src || img.dataset.lazySrc;
+            const url = new mw.Uri(src);
             if (url.host !== "img.moegirl.org.cn") {
                 continue;
             }
-            img.src = img.src.replace("/thumb/", "/").replace(/\.svg\/[^/]+\.svg\.png$/, ".svg");
+            img.src = src.replace("/thumb/", "/").replace(/\.svg\/[^/]+\.svg\.png$/, ".svg");
             img.removeAttribute("srcset");
             img.removeAttribute("data-lazy-src");
             img.removeAttribute("data-lazy-srcset");
