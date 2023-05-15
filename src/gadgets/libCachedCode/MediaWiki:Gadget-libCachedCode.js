22b7bf5832ce2db3eb30617a215a9b1b9f2ed6bd
AnnAngela <naganjue@vip.qq.com>
2023-05-15 11:35:17 +0800

    feat(Gadget-libCachedCode): Add caching mechanism and support via `libCachedCode`
    Add caching mechanism for `libCachedCode` to support retrieving cached code and injecting it to the page, with support for `script` and `css` types. Also, add `libCachedCode` as a readonly global variable and a hidden gadget in `.eslintrc.yaml`, `settings.json`, and `definition.yaml`. The `MediaWiki:Gadget-libCachedCode.js` script provides methods to get cached code, obtain the URL for cached code and inject cached code to the page.

---
+++ src/gadgets/libCachedCode/MediaWiki:Gadget-libCachedCode.js
@@ -0,0 +1,35 @@
+"use strict";
+(() => {
+    const codeToUrl = (code) => {
+        const blob = new Blob([code], { type: "text/plain" });
+        return URL.createObjectURL(blob);
+    };
+    const getCachedCode = async (url) => {
+        const cachedCode = localStorage.getItem(`AnnTools-libCachedCode:${url}`);
+        if (cachedCode) {
+            return cachedCode;
+        }
+        const freshCode = await (await fetch(url)).text();
+        localStorage.setItem(`AnnTools-libCachedCode:${url}`, freshCode);
+        return freshCode;
+    };
+    const getCachedCodeUrl = async (url) => codeToUrl(await getCachedCode(url));
+    const injectCachedCode = async (url, _type) => {
+        const type = _type.toLowerCase();
+        if (["script", "javascript", "js"].includes(type)) {
+            const script = document.createElement("script");
+            script.src = await getCachedCodeUrl(url);
+            document.head.append(script);
+            return;
+        }
+        if (["css", "style"].includes(type)) {
+            mw.loader.addStyleTag(await getCachedCode(url));
+            return;
+        }
+    };
+    window.libCachedCode = {
+        getCachedCode,
+        getCachedCodeUrl,
+        injectCachedCode,
+    };
+})();
