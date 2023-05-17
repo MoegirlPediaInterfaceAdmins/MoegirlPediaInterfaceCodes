fcef7a84239d0d74eabc1cc26063dace21780c3b
AnnAngela <naganjue@vip.qq.com>
2023-05-17 15:20:35 +0800

    fix(Gadget-wikieditor-highlight): Fix bug

--- src/gadgets/wikieditor-highlight/MediaWiki:Gadget-wikieditor-highlight.js
+++ src/gadgets/wikieditor-highlight/MediaWiki:Gadget-wikieditor-highlight.js
@@ -2,76 +2,57 @@
 // 本页面大部分内容均直接或间接修改自[[MW:Extension:CodeMirror]]
 (async () => {
     await $.ready;
-    if (!["edit", "submit"].includes(mw.config.get("wgAction")) || mw.config.get("wgPageContentModel") !== "wikitext") { return; }
+    if (!["edit", "submit"].includes(mw.config.get("wgAction")) || mw.config.get("wgPageContentModel") !== "wikitext") {
+        return;
+    }
 
-    let cm, $doc, state = JSON.parse(localStorage.getItem("wikieditor-codemirror"));
+    const localObjectStorage = new LocalObjectStorage("wikieditor-highlight");
+    let cm, $doc, state = localObjectStorage.getItem("wikieditor-codemirror", false);
     const $textarea = $("#wpTextbox1");
     const isAdvanced = ["loading", "loaded", "executing", "ready"].includes(mw.loader.getState("ext.wikiEditor"));
     const ns = mw.config.get("wgNamespaceNumber");
     const init = async () => {
-        await libCachedCode.injectCachedCode("https://npm.elemecdn.com/codemirror@5.65.1/lib/codemirror.css", "style");
-        // 由于 unpkg 不提供 gh 镜像，故通过 [[MediaWiki:Gadget-wikieditor-highlight_codemirror-mediawiki.css]] 提供
-        // mw.loader.load("//fastly.jsdelivr.net/gh/bhsd-harry/codemirror-mediawiki@1.1.6/mediawiki.min.css", "text/css");
-
-        // 通过 [[MediaWiki:Gadget-wikieditor-highlight.css]] 提供
-        /* mw.loader.addStyleTag("#wikiEditor-ui-toolbar .menu { position: relative; z-index: 5; }" +
-            '.CodeMirror pre { font-family: Monaco, Menlo, "Ubuntu Mono", Consolas, "source-code-pro", monospace; }' +
-            "#wpTextbox1 + .CodeMirror { font-size: 13px; line-height: 1.5; }" +
-            ".CodeMirror-matchingbracket { box-shadow: 0 0 0 2px #9aef98; }" +
-            ".CodeMirror-nonmatchingbracket { box-shadow: 0 0 0 2px #eace64; }",
-        ); */
         const $search = $(".group-search a");
-
-        // 通过下方代码和 [[MediaWiki:Gadget-wikieditor-highlight_codemirror-mediawiki.js]] 提供
-        /* const getExt = $.get({
-            dataType: "script", cache: true,
-            url: "//fastly.jsdelivr.net/combine/npm/codemirror@5.65.1/lib/codemirror.min.js,npm/codemirror@5.65.1/addon/edit/matchbrackets.min.js,npm/wikiplus-highlight@2.22/matchtags.min.js",
-        }).then(() => Promise.all([$.get({
-            dataType: "script", cache: true,
-            url: "//fastly.jsdelivr.net/gh/bhsd-harry/codemirror-mediawiki@1.1.6/mediawiki.min.js",
-        })].concat(ns === 274 ? [$.get({
-            dataType: "script", cache: true,
-            url: "//fastly.jsdelivr.net/npm/codemirror@5.65.1/mode/javascript/javascript.min.js",
-        }), $.get({
-            dataType: "script", cache: true,
-            url: "//fastly.jsdelivr.net/npm/codemirror@5.65.1/mode/css/css.min.js",
-        })] : []))); */
-
-        // 由于 unpkg 不提供 gh 镜像，故通过 [[MediaWiki:Gadget-wikieditor-highlight_CodeMirror.js]] 提供
-        /* const getJSON = $.get({
-            dataType: "json", cache: true,
-            url: "//fastly.jsdelivr.net/gh/bhsd-harry/LLWiki@2.16/otherwiki/gadget-CodeMirror.json",
-        }).then((config) => {
-            if (ns === 274) {
-                $.extend(config.tags, { script: true, style: true });
-                $.extend(config.tagModes, { script: "javascript", style: "css" });
-            }
-            window.mwConfig = config;
-        });
-        await Promise.all([getJSON, getExt]); */
-        await Promise.all([
+        await libCachedCode.batchInjectCachedCode([
+            "https://npm.elemecdn.com/codemirror@5.65.1/lib/codemirror.css",
+            "https://npm.elemecdn.com/@bhsd/codemirror-mediawiki@1.1.12/mediawiki.css",
+        ], "style");
+        await libCachedCode.batchInjectCachedCode([
             "https://npm.elemecdn.com/codemirror@5.65.1/lib/codemirror.js",
             "https://npm.elemecdn.com/codemirror@5.65.1/addon/edit/matchbrackets.js",
             "https://npm.elemecdn.com/wikiplus-highlight@2.22.5/matchtags.js",
-        ].map((url) => libCachedCode.injectCachedCode(url, "script")));
-        window.codemirrorMediawiki(CodeMirror);
-        if (ns === 274) {
-            await Promise.all([
+        ], "script");
+        await libCachedCode.batchInjectCachedCode([
+            "https://npm.elemecdn.com/@bhsd/codemirror-mediawiki@1.1.12/mediawiki.js",
+            ...ns === 274 ? [
                 "https://npm.elemecdn.com/codemirror@5.65.1/mode/javascript/javascript.js",
                 "https://npm.elemecdn.com/codemirror@5.65.1/mode/css/css.js",
-            ].map((url) => libCachedCode.injectCachedCode(url, "script")));
+            ] : [],
+        ], "script");
+        const mwConfig = await libCachedCode.getCachedCode("https://npm.elemecdn.com/@bhsd/codemirror-mediawiki@1.1.12/config.json");
+        if (ns === 274) {
+            $.extend(mwConfig.tags, {
+                script: true, style: true,
+            });
+            $.extend(mwConfig.tagModes, {
+                script: "javascript", style: "css",
+            });
         }
         if (isAdvanced) {
             cm = new CodeMirror($textarea.parent()[0], {
-                mode: "text/mediawiki", mwConfig: window.mwConfig,
+                mode: "text/mediawiki", mwConfig,
                 lineWrapping: true, lineNumbers: true, readOnly: $textarea.prop("readonly"),
-                matchBrackets: { bracketRegex: /[{}[\]]/ }, matchTags: true,
+                matchBrackets: {
+                    bracketRegex: /[{}[\]]/,
+                }, matchTags: true,
             });
         } else {
             cm = CodeMirror.fromTextArea($textarea[0], {
-                mode: "text/mediawiki", mwConfig: window.mwConfig,
+                mode: "text/mediawiki", mwConfig,
                 lineWrapping: true, lineNumbers: true, readOnly: $textarea.prop("readonly"),
-                matchBrackets: { bracketRegex: /[{}[\]]/ }, matchTags: true,
+                matchBrackets: {
+                    bracketRegex: /[{}[\]]/,
+                }, matchTags: true,
             });
             cm.setSize(null, $textarea.height());
         }
@@ -79,25 +60,46 @@
         $doc = $(cm.getWrapperElement());
         $.valHooks.textarea = {
             get: (ele_1) => ele_1 === $textarea[0] && state ? cm.getValue() : ele_1.value,
-            set: (ele_3, val) => { ele_3 === $textarea[0] && state ? cm.setValue(val) : ele_3.value = val; },
+            set: (ele_3, val) => {
+                ele_3 === $textarea[0] && state ? cm.setValue(val) : ele_3.value = val;
+            },
         };
-        if (mw.loader.getState("jquery.ui.resizable") === "ready") { $doc.resizable({ handles: "s" }); }
-        if ($search.length === 0) { return; }
-        cm.addKeyMap({ "Ctrl-F": function () { $search.trigger("click"); }, "Cmd-F": function () { $search.trigger("click"); } });
+        if (mw.loader.getState("jquery.ui.resizable") === "ready") {
+            $doc.resizable({
+                handles: "s",
+            });
+        }
+        if ($search.length === 0) {
+            return;
+        }
+        cm.addKeyMap({
+            "Ctrl-F": () => {
+                $search.trigger("click");
+            },
+            "Cmd-F": () => {
+                $search.trigger("click");
+            },
+        });
     };
-    if (state === null || !isAdvanced) { state = true; }
+    if (state === null || !isAdvanced) {
+        state = true;
+    }
     if (!isAdvanced) {
         init();
         return;
     }
     const $form = $(document.editform);
-    const btn = new OO.ui.ButtonWidget({ classes: ["tool"], icon: "highlight", framed: false, title: "代码高亮开关" }).on("click", () => {
+    const btn = new OO.ui.ButtonWidget({
+        classes: ["tool"], icon: "highlight", framed: false, title: "代码高亮开关",
+    }).on("click", () => {
         if (cm) {
             $doc.toggle();
             // eslint-disable-next-line no-use-before-define
             update();
+        } else {
             // eslint-disable-next-line no-use-before-define
-        } else { initAndUpdate(); }
+            initAndUpdate();
+        }
     });
     const fn = {
         getSelection: () => cm.getSelection(),
@@ -109,7 +111,9 @@
         getCaretPosition: (options) => {
             const caretPos = cm.indexFromPos(cm.getCursor("from")),
                 endPos = cm.indexFromPos(cm.getCursor("to"));
-            if (options.startAndEnd) { return [caretPos, endPos]; }
+            if (options.startAndEnd) {
+                return [caretPos, endPos];
+            }
             return caretPos;
         },
         scrollToCaretPosition: function () {
@@ -117,13 +121,17 @@
             return this;
         },
     };
-    const submit = () => { $textarea[0].value = cm.getValue(); };
+    const submit = () => {
+        $textarea[0].value = cm.getValue();
+    };
     const shared = () => {
         btn.$element.toggleClass("tool-active");
         if (state) {
             cm.setValue($textarea[0].value);
             cm.setSize(null, $textarea.height());
-        } else { $textarea[0].value = cm.getValue(); }
+        } else {
+            $textarea[0].value = cm.getValue();
+        }
         $textarea.toggle();
         $form[state ? "on" : "off"]("submit", submit);
         if ($textarea.textSelection) {
@@ -132,10 +140,12 @@
     };
     const update = () => {
         state = !state;
-        localStorage.setItem("wikieditor-codemirror", state);
+        localObjectStorage.setItem("wikieditor-codemirror", state);
         shared();
     };
-    const initAndUpdate = () => { init().then(update); };
+    const initAndUpdate = () => {
+        init().then(update);
+    };
     const group = $("#wikiEditor-section-main > .group-insert")[0];
     $textarea.on("wikiEditor-toolbar-doneInitialSections", () => {
         btn.$element.appendTo("#wikiEditor-section-main > .group-insert");
@@ -143,5 +153,7 @@
     if (group && !group.contains(btn.$element[0])) {
         $textarea.trigger("wikiEditor-toolbar-doneInitialSections");
     }
-    if (state) { mw.loader.using("ext.wikiEditor").then(init).then(shared); }
+    if (state) {
+        mw.loader.using("ext.wikiEditor").then(init).then(shared);
+    }
 })();
