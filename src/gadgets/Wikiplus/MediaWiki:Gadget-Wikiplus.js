4b5d1729c9f36bfa27e9cc9a549c58473e4cfe22
AnnAngela <naganjue@vip.qq.com>
2023-05-20 15:41:15 +0800

    feat(gadgets): remove Wikiplus eslint and activation script
    This commit adds script to add Wikiplus QuickEdit button and removes eslint and activation script from Wikiplus gadget. The script uses libCachedCode to inject cached code.

--- src/gadgets/Wikiplus/MediaWiki:Gadget-Wikiplus.js
+++ src/gadgets/Wikiplus/MediaWiki:Gadget-Wikiplus.js
@@ -1,1 +1,21 @@
-mw.loader.load("https://npm.elemecdn.com/wikiplus-core@4");
+"use strict";
+(async () => {
+    await libCachedCode.injectCachedCode("https://npm.elemecdn.com/wikiplus-core@4.0.11/dist/Main.js", "script");
+    const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+    if (mw.config.get("skin") !== "moeskin" || !document.querySelector("#ca-edit")) {
+        return;
+    }
+    let wikiplusEditTopBtn = document.querySelector("#Wikiplus-Edit-TopBtn");
+    while (!wikiplusEditTopBtn) {
+        await sleep(100);
+        // eslint-disable-next-line require-atomic-updates
+        wikiplusEditTopBtn = document.querySelector("#Wikiplus-Edit-TopBtn");
+    }
+    mw.hook("moeskin.pagetools").add(({ addPageToolsButton }) => {
+        const $btn = addPageToolsButton('<span style="align-self:center;font:0.7em bold;">W+</span>', "快速编辑");
+        $btn.attr("id", "ca-wikiplus").on("click", () => {
+            wikiplusEditTopBtn.click();
+        });
+    });
+    $(".page-tool-link#ca-wikiplus").insertAfter($(".page-tool-link#ca-edit"));
+})();
