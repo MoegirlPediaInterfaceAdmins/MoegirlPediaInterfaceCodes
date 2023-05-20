8a9956f8acda981f7de4b0472c0b53959195cb8a
AnnAngela <naganjue@vip.qq.com>
2023-05-20 15:41:39 +0800

    feat: Improve wikiplus-highlight gadget
    Deleted the `.eslintrc.yaml` configuration file and ignored the `MediaWiki:Gadget-wikiplus-highlight.js` file, and updated the code with an improved version that adds caching for better performance.

--- src/gadgets/wikiplus-highlight/MediaWiki:Gadget-wikiplus-highlight.js
+++ src/gadgets/wikiplus-highlight/MediaWiki:Gadget-wikiplus-highlight.js
@@ -1,1 +1,4 @@
-mw.loader.load("https://npm.elemecdn.com/wikiplus-highlight@latest");
+"use strict";
+(async () => {
+    await libCachedCode.injectCachedCode("https://npm.elemecdn.com/wikiplus-highlight@2.59.5/dist/main.min.js", "script");
+})();
