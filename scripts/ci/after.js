008e032c1cd008fcfc19cdcb6079e320895b392f
AnnAngela <naganjue@vip.qq.com>
2023-05-23 09:36:19 +0800

    feat(ci): Backup and restore package-lock.json file
    Add functionality to backup and restore package-lock.json file before and after a job pipeline, respectively. Now before running the pipeline, package-lock.json is backed up to a temporary directory, and during recovering, the backup file is retrieved first and then the file is recovered from git. The backuped file is used if it exists otherwise git is used to restore the file.

--- scripts/ci/after.js
+++ scripts/ci/after.js
@@ -2,8 +2,24 @@ import console, { originalConsole } from "../modules/console.js";
 originalConsole.info("=".repeat(120));
 console.info("Initialization done.");
 import git from "../modules/git.js";
+import mkdtmp from "../modules/mkdtmp.js";
+import fs from "fs";
+import path from "path";
 
 const packageLockFile = "package-lock.json";
+
 console.info("Start to recover", packageLockFile);
-await git.checkout(packageLockFile);
+const tmpdir = await mkdtmp({
+    random: false,
+});
+const backupedPackageLockFile = path.join(tmpdir, packageLockFile);
+console.info("Start to check backup file:", backupedPackageLockFile);
+const backupedPackageLockFileExists = await fs.promises.access(backupedPackageLockFile).then(() => true).catch(() => false);
+if (backupedPackageLockFileExists) {
+    console.info("Backup file exists, use it to recover.");
+    await fs.promises.cp(backupedPackageLockFile, packageLockFile, { force: true, preserveTimestamps: true });
+} else {
+    console.info("Backup file unexists, use `git` to recover.");
+    await git.checkout(packageLockFile);
+}
 console.info("Done.");
--- scripts/ci/before.js
+++ scripts/ci/before.js
@@ -2,15 +2,26 @@ import console, { originalConsole } from "../modules/console.js";
 console.info("Initialization done.");
 import jsonModule from "../modules/jsonModule.js";
 import exec from "../modules/exec.js";
+import mkdtmp from "../modules/mkdtmp.js";
+import fs from "fs";
+import path from "path";
+
+const packageLockFile = "package-lock.json";
 
 const localRegistry = await exec("npm config get registry");
 const registries = [
     "https://registry.npmjs.org/",
     "https://mirrors.cloud.tencent.com/npm/",
 ].filter((registry) => registry !== localRegistry);
-const packageLockFile = "package-lock.json";
 console.info("localRegistry:", localRegistry);
 console.info("registries:", registries);
+console.info("Start to backup", packageLockFile);
+const tmpdir = await mkdtmp({
+    random: false,
+});
+const backupedPackageLockFile = path.join(tmpdir, packageLockFile);
+await fs.promises.cp(packageLockFile, backupedPackageLockFile, { force: true, preserveTimestamps: true });
+console.info("backup:", backupedPackageLockFile);
 console.info("Start to read", packageLockFile);
 const packageLockFileContent = await jsonModule.readFile(packageLockFile);
 console.info("Start to modified resolved path for packages");
