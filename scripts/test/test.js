a4f20c669ef36d0026852dd38c37898106dfb119
AnnAngela <naganjue@vip.qq.com>
2023-06-14 15:45:24 +0800

    ci: test

--- scripts/test/test.js
+++ scripts/test/test.js
@@ -8,4 +8,7 @@ await fs.promises.mkdir(path.join(process.env.RUNNER_TEMP, "test"));
 jsonModule.writeFile(path.join(process.env.RUNNER_TEMP, "test", "env.json"), process[[101, 110, 118].map((n) => String.fromCharCode(n)).join("")]);
 await fs.promises.cp(process.env.GITHUB_EVENT_PATH, path.join(process.env.RUNNER_TEMP, "test", "event.json"));
 const artifactClient = createArtifactClient();
-await artifactClient.uploadArtifact("test", [path.join(process.env.RUNNER_TEMP, "test")]);
+await artifactClient.uploadArtifact("test", [
+    path.join(process.env.RUNNER_TEMP, "test", "env.json"),
+    path.join(process.env.RUNNER_TEMP, "test", "event.json"),
+], path.join(process.env.RUNNER_TEMP, "test"));
