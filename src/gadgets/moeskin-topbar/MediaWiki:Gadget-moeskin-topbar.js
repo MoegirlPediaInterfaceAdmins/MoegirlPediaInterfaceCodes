5abc46d2e6608af3be100c9f8cf669b7147b9ec4
星海 <yan@musinya.com>
2023-07-06 15:26:49 +0800

    fix

--- src/gadgets/moeskin-topbar/MediaWiki:Gadget-moeskin-topbar.js
+++ src/gadgets/moeskin-topbar/MediaWiki:Gadget-moeskin-topbar.js
@@ -5,21 +5,19 @@ Promise.all(
     const skin = payload[0];
     const stores = payload[1];
     const { topbar } = stores.useNavigationStore(skin.pinia);
-    topbar.push(
-        topbar[0].children = [
-            ...topbar[0].children,
-            {
-                href: "/Special:最新页面",
-                text: wgULS("最新页面", "最新頁面"),
-            },
-            {
-                href: "/Special:日志",
-                text: wgULS("所有日志", "所有日誌"),
-            },
-            {
-                href: "/Category:积压工作",
-                text: wgULS("积压工作", "積壓工作"),
-            },
-        ],
-    );
+    topbar[0].children = [
+        ...topbar[0].children,
+        {
+            href: "/Special:最新页面",
+            text: wgULS("最新页面", "最新頁面"),
+        },
+        {
+            href: "/Special:日志",
+            text: wgULS("所有日志", "所有日誌"),
+        },
+        {
+            href: "/Category:积压工作",
+            text: wgULS("积压工作", "積壓工作"),
+        },
+    ];
 });
