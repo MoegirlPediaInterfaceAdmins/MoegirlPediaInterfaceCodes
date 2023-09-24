ee7233d946cd52c65dca57bc414e3b69eba40e6a
BearBin <20369414+BearBin1215@users.noreply.github.com>
2023-09-24 23:38:50 +0800

    feat: 新工具讨论页复制固定链接

---
+++ src/gadgets/SectionPermanentLink/MediaWiki:Gadget-SectionPermanentLink.js
@@ -0,0 +1,41 @@
+"use strict";
+$(() => {
+    if (mw.config.get("wgNamespaceNumber") % 2 === 1) {
+        const inHistory = !document.getElementsByClassName("mw-editsection")[0];
+        const buttunText = wgULS("固定链接", "固定連結");
+        $("#mw-content-text .mw-parser-output h2").each((_, ele) => {
+            const $ele = $(ele);
+            const $divider = $('<span class="mw-editsection-divider"> | </span>');
+            const $permanentLink = $(`<a data-thread-id="${$ele.find(".mw-headline").attr("id")}">${buttunText}</a>`);
+            if (!inHistory) {
+                $(ele)
+                    .find(".mw-editsection-bracket")
+                    .first()
+                    .after($divider)
+                    .after($permanentLink);
+                const $marButton = $ele.find(".AnnTools_MarkAsResolved");
+                if ($marButton[0]) {
+                    const $marDivider = $marButton.next(".mw-editsection-divider");
+                    if ($marDivider.length > 0) {
+                        $marDivider.after($divider).after($permanentLink);
+                    }
+                }
+            } else {
+                const permanentLinkButton = $('<span class="mw-editsection"></span>');
+                permanentLinkButton.append(
+                    '<span class="mw-editsection-bracket">[</span>',
+                    $permanentLink,
+                    '<span class="mw-editsection-bracket">]</span>',
+                );
+                $ele.find(".mw-headline").after(permanentLinkButton);
+            }
+            $permanentLink.on("click", () => {
+                navigator.clipboard.writeText(`[[Special:PermanentLink/${mw.config.get("wgRevisionId")}#${$permanentLink.data("thread-id")}]]`);
+                $permanentLink.text(wgULS("复制成功", "復製成功"));
+                setTimeout(() => {
+                    $permanentLink.text(buttunText);
+                }, 2000);
+            });
+        });
+    }
+});
