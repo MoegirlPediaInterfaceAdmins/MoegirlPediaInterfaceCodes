4a0cd4690b14f2a0608796159cb30b1583eec2cb
AnnAngela <naganjue@vip.qq.com>
2023-10-31 09:22:35 +0800

    feat(Gadget-libCachedCode): 增加缓存过期清理功能

--- src/gadgets/libCachedCode/MediaWiki:Gadget-libCachedCode.js
+++ src/gadgets/libCachedCode/MediaWiki:Gadget-libCachedCode.js
@@ -1,17 +1,22 @@
 "use strict";
 (() => {
+    const localObjectStorage = new LocalObjectStorage("AnnTools-libCachedCode");
+    for (const i of Object.keys(localStorage)) { // 移除旧版本缓存
+        if (i.startsWith("AnnTools-libCachedCode")) {
+            localStorage.removeItem(i);
+        }
+    }
     const codeToUrl = (code) => {
         const blob = new Blob([code], { type: "text/plain" });
         return URL.createObjectURL(blob);
     };
     const getCachedCode = async (url) => {
-        const cachedCode = localStorage.getItem(`AnnTools-libCachedCode:${url}`);
-        if (cachedCode) {
-            return cachedCode;
+        let { code } = localObjectStorage.getItem(`${url}`) || {}; // 读取缓存
+        if (!code) { // 如无则获取数据
+            code = await (await fetch(url)).text();
         }
-        const freshCode = await (await fetch(url)).text();
-        localStorage.setItem(`AnnTools-libCachedCode:${url}`, freshCode);
-        return freshCode;
+        localObjectStorage.setItem(`AnnTools-libCachedCode:${url}`, { code, timestamp: Date.now() }); // 设置缓存
+        return code;
     };
     const getCachedCodeUrl = async (url) => codeToUrl(await getCachedCode(url));
     const injectCachedCode = async (url, _type) => {
@@ -32,6 +37,13 @@
         }
     };
     const batchInjectCachedCode = (urls, type) => Promise.all(urls.map((url) => injectCachedCode(url, type)));
+    // 移除过期缓存（30 天内无使用）
+    const expired = Date.now() - 30 * 24 * 60 * 60 * 1000;
+    for (const k of localObjectStorage._getAllKeys()) {
+        if (localObjectStorage.getItem(k).timestamp < expired) {
+            localObjectStorage.removeItem(k);
+        }
+    }
     window.libCachedCode = {
         getCachedCode,
         getCachedCodeUrl,
