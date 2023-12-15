17f91d7e273fe413e346aac592d0a26c755be1e7
AnnAngela <naganjue@vip.qq.com>
2023-12-15 10:00:00 +0800

    ci: 简化流程

---
+++ infoAboutFailure.sh
@@ -0,0 +1,7 @@
+echo "如果代码检查失败，你可以通过以下方式查看错误原因："
+echo "1. 如果本次提交属于 Pull Request，你可以在该 PR 的 【Files changed】 标签页查看错误原因；"
+echo "2. 如果本次提交属于直接推送 commit，你可以在该 commit 页查看错误原因。"
+echo "你可能需要下拉到对应页面的最底部来查看未被修改的文件的错误原因。"
+if [ $# -eq 1 ]; then
+    exit $1
+fi
