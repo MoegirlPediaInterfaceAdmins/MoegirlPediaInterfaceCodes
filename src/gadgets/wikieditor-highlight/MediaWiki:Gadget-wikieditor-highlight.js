c2ab02d97a38262179cd63a75ff132a0b5b47376
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-02-01 11:32:28 +0800

    Update MediaWiki:Gadget-wikieditor-highlight.js
    大更新

--- src/gadgets/wikieditor-highlight/MediaWiki:Gadget-wikieditor-highlight.js
+++ src/gadgets/wikieditor-highlight/MediaWiki:Gadget-wikieditor-highlight.js
@@ -18,7 +18,7 @@
                 const script = document.createElement("script");
                 script.addEventListener("load", resolve);
                 script.type = "module";
-                script.src = "https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki/mw/dist/base.js";
+                script.src = "https://testingcf.jsdelivr.net/npm/@bhsd/codemirror-mediawiki/dist/mw.min.js";
                 document.head.append(script);
             });
         }
