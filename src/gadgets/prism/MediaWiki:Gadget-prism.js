a1a8c7f87524df6f8c60b836e03b4f9899a97c3c
AnnAngela <naganjue@vip.qq.com>
2024-03-25 17:51:32 +0800

    feat: prism prepare

---
+++ src/gadgets/prism/MediaWiki:Gadget-prism.js
@@ -0,0 +1,61 @@
+"use strict";
+$(() => {
+    /* window.Prism = { manual: true };
+    const acceptsLangs = {
+        // css
+        css: "css",
+        "sanitized-css": "css",
+        // javascript
+        javascript: "javascript",
+        js: "javascript",
+        // json
+        json: "json",
+        json5: "json",
+        webmanifest: "json",
+        // latex
+        latex: "latex",
+        tex: "latex",
+        context: "latex",
+        // lua
+        lua: "lua",
+        scribunto: "lua",
+        // markup
+        markup: "markup",
+        htm: "markup",
+        html: "markup",
+        mathml: "markup",
+        svg: "markup",
+        xml: "markup",
+        ssml: "markup",
+        atom: "markup",
+        rss: "markup",
+        // php
+        php: "php",
+        // regex
+        regex: "regex",
+        // typescript
+        typescript: "typescript",
+        ts: "typescript",
+        // wiki
+        wiki: "wiki",
+        wikitext: "wiki",
+        mediawiki: "wiki",
+        mw: "wiki",
+        // yaml
+        yaml: "yaml",
+        yml: "yaml",
+    };
+    mw.hook("wikipage.content").add(($content) => {
+        const wgPageContentModel = mw.config.get("wgPageContentModel", "").toLowerCase();
+        const langSet = new Set();
+        if (Reflect.has(acceptsLangs, wgPageContentModel)) {
+            $content.find(".mw-code").addClass(`prettyprint lang-${acceptsLangs[wgPageContentModel]}`);
+        }
+        if (wgPageContentModel === "wikitext") {
+            //language-
+            for (const ele of $content.find("pre, code")) {
+
+            }
+        }
+    }); */
+});
