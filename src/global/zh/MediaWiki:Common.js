c8b2faccc18904d97da04aa1ba61868da2e0f29f
机智的小鱼君 <44761872+dragon-fish@users.noreply.github.com>
2024-04-10 23:37:21 +0800

    chore: compatibility

--- src/global/zh/MediaWiki:Common.js
+++ src/global/zh/MediaWiki:Common.js
@@ -94,7 +94,7 @@
         const domain = url.host;
         const crossDomain = $("<div/>");
         const anchor = $("<a/>");
-        crossDomain.html('<span class="xicon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" fill="currentColor"></path></svg></span>');
+        crossDomain.html('<span class="moe-icon"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 256 512"><path d="M31.7 239l136-136c9.4-9.4 24.6-9.4 33.9 0l22.6 22.6c9.4 9.4 9.4 24.6 0 33.9L127.9 256l96.4 96.4c9.4 9.4 9.4 24.6 0 33.9L201.7 409c-9.4 9.4-24.6 9.4-33.9 0l-136-136c-9.5-9.4-9.5-24.6-.1-34z" fill="currentColor"></path></svg></span>');
         anchor.attr("href", `${url}`).text(`${link} 【来自 ${domain} 的跨站重定向】`);
         crossDomain.append(anchor);
         $("#tagline").after(crossDomain);
