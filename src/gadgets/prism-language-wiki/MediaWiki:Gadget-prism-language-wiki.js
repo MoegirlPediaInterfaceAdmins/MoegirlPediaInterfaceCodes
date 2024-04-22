ba203efd0047c107b0ec59ef9e58e15109376cc5
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-04-23 00:19:25 +0800

    fix(gadget-prism): no unicode flag (#465)

--- src/gadgets/prism-language-wiki/MediaWiki:Gadget-prism-language-wiki.js
+++ src/gadgets/prism-language-wiki/MediaWiki:Gadget-prism-language-wiki.js
@@ -5,6 +5,7 @@
     const wiki = {};
     Prism.languages.wiki = wiki;
 
+    await libCachedCode.injectCachedCode("/MediaWiki:Gadget-wikiparser-node.js?action=raw&ctype=text/javascript", "script");
     Parser.config = await (await fetch("/MediaWiki:Gadget-prism-language-wiki.json?action=raw&ctype=application/json")).json();
     const keyword = "keyword",
         url = "url",
