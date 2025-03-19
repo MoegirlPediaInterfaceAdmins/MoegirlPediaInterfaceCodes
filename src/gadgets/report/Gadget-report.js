3e899e729c5bb4e8ee954219ce1a3e2ec591c891
dragon-fish <dragon-fish@qq.com>
2025-03-19 19:08:52 +0800

    fix: css conflict

--- src/gadgets/report/Gadget-report.js
+++ src/gadgets/report/Gadget-report.js
@@ -442,14 +442,16 @@ $(() => {
         windowManager.openWindow(reportDialog);
     };
 
+    const WarningIcon = (props = {}) => $("<svg  xmlns=\"http://www.w3.org/2000/svg\"  width=\"24\"  height=\"24\"  viewBox=\"0 0 24 24\"  fill=\"currentColor\"  class=\"icon icon-tabler icons-tabler-filled icon-tabler-alert-triangle\"><path stroke=\"none\" d=\"M0 0h24v24H0z\" fill=\"none\"/><path d=\"M12 1.67c.955 0 1.845 .467 2.39 1.247l.105 .16l8.114 13.548a2.914 2.914 0 0 1 -2.307 4.363l-.195 .008h-16.225a2.914 2.914 0 0 1 -2.582 -4.2l.099 -.185l8.11 -13.538a2.914 2.914 0 0 1 2.491 -1.403zm.01 13.33l-.127 .007a1 1 0 0 0 0 1.986l.117 .007l.127 -.007a1 1 0 0 0 0 -1.986l-.117 -.007zm-.01 -7a1 1 0 0 0 -.993 .883l-.007 .117v4l.007 .117a1 1 0 0 0 1.986 0l.007 -.117v-4l-.007 -.117a1 1 0 0 0 -.993 -.883z\" /></svg>", props);
+
     const $extraReportArea = $("<div>", {
         "class": "report-article-footer",
         style: "text-align: center; margin-top: 1em;",
     });
     const $extraReportButton = $("<a>", {
         "class": "report-article-button",
-        text: wgULS("违规举报 & 页面反馈", "違規舉報 & 頁面反饋"),
         style: "display: inline-block; padding: 0.5em 1em; background-color: var(--theme-danger-color, #ff4848); color: var(--theme-accent-link-color, #fff); border-radius: 100vmax; cursor: pointer;",
+        text: wgULS("违规举报 & 页面反馈", "違規舉報 & 頁面反饋"),
         click: initReport,
     });
     $extraReportArea.append($extraReportButton);
@@ -457,10 +459,9 @@ $(() => {
     switch (mw.config.get("skin")) {
         case "moeskin":
             mw.hook("moeskin.pagetools").add(({ addPageToolsButton }) => {
-                /** @type {JQuery<Element>} */
-                const $icon = $("<svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 34.88 29.9\"><defs><style>.cls-1{fill:#ff4848;}.cls-2{fill:#fff;}</style></defs><g id=\"图层_2\" data-name=\"图层 2\"><g id=\"图层_1-2\" data-name=\"图层 1\"><path class=\"cls-1\" d=\"M31.56,29.9H3.26a3.32,3.32,0,0,1-2.8-5L14.66,1.6a3.25,3.25,0,0,1,5.6,0l14.1,23.3A3.25,3.25,0,0,1,31.56,29.9Z\"/><path class=\"cls-2\" d=\"M19.25,9.76l-.42,9.6a1.42,1.42,0,0,1-2.78,0l-.42-9.6c0-2.11,3.62-2.11,3.62,0Z\"/><path class=\"cls-2\" d=\"M17.44,26.7a2,2,0,0,1,0-4.09h0a2,2,0,1,1,0,4.09Z\"/></g></g></svg>");
-                const $btn = addPageToolsButton($icon, wgULS("页面举报&反馈", "頁面舉報&反饋"), "extra");
+                const $btn = addPageToolsButton(WarningIcon(), wgULS("页面举报&反馈", "頁面舉報&反饋"), "extra");
                 $btn.on("click", initReport);
+                $btn.css("--theme-button-color", "var(--theme-danger-color)");
             });
             $("#moe-after-content").prepend($extraReportArea);
             break;
