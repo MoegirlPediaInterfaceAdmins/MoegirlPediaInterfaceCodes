48ac3eed0d3ed8944a9fb6a1aceb858c61247d89
bearbin1215 <bearbin1215@qq.com>
2025-09-04 09:42:12 +0800

    fix: #698

--- src/gadgets/shortlink/Gadget-shortlink.js
+++ src/gadgets/shortlink/Gadget-shortlink.js
@@ -109,7 +109,7 @@ $(() => {
             });
 
             // 点击复制操作
-            $("#p-sl-list a[data-type]").on("click", async () => {
+            $("#p-sl-list a[data-type]").on("click", async function () {
                 try {
                     await navigator.clipboard.writeText(this.dataset.copyContent);
                     markStatus(this, true, true);
