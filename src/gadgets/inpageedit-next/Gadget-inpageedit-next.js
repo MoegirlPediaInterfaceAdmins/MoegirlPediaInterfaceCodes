91beb765db00535c591d05a842a751f2ae097d09
xiaoyujun <xiaoyujun@aojiaogame.com>
2025-11-15 01:53:10 +0800

    refactor: implement ipe plugin

--- src/gadgets/inpageedit-next/Gadget-inpageedit-next.js
+++ src/gadgets/inpageedit-next/Gadget-inpageedit-next.js
@@ -1,3 +1,5 @@
+/// <reference types="@inpageedit/core" />
+
 "use strict";
 
 window.RLQ ||= [];
@@ -7,7 +9,7 @@ window.RLQ.push([
         const isLoadedV2 = mw.loader.getState("ext.gadget.InPageEdit-v2") === "ready" || window.InPageEdit?.__loaded === true;
         if (isLoadedV2) {
             mw.notify("无法同时安装 InPageEdit v2 和 InPageEdit NEXT，请卸载其中之一。", {
-                type: "warning",
+                type: "warn",
             });
             return;
         }
@@ -20,47 +22,75 @@ window.RLQ.push([
             }),
         );
 
-        mw.hook("InPageEdit.ready").add((ctx) => {
-            const wgPageName = mw.config.get("wgRelevantPageName");
-            const wgRevisionId = mw.config.get("wgRevisionId");
-            switch (mw.config.get("skin")) {
-                case "vector-2022": {
-                    $("#ca-edit").after(
-                        $("<li>", {
-                            id: "ca-quick-edit",
-                            "class": "vector-tab-noicon mw-list-item",
-                        }).append(
-                            $("<a>", {
-                                href: "javascript:void(0)",
-                                text: typeof Wikiplus !== "undefined" ? "快速编辑(IPE)" : "快速编辑",
-                            }).on("click", () => {
-                                ctx.quickEdit({
-                                    page: wgPageName,
-                                    revision: wgRevisionId || undefined,
-                                });
-                            }),
-                        ),
-                    );
-                    break;
-                }
-                case "moeskin":
-                default: {
-                    mw.hook("moeskin.pagetools").add(({ addPageToolsButton }) => {
-                        const $btn = addPageToolsButton('<span style="align-self:center;font:0.7em bold;">IPE</span>', "快速编辑");
-                        $btn.attr({
-                            id: "ca-inpageedit",
-                            href: "javascript:void(0)",
-                        }).on("click", () => {
-                            ctx.quickEdit({
-                                page: wgPageName,
-                                revision: wgRevisionId || undefined,
-                            });
-                        });
+        // Plugin: Skin Edit Button
+        mw.hook("InPageEdit.ready").add((ipe) => {
+            ipe.plugin({
+                name: "skin-edit-button",
+                inject: ["quickEdit", "wiki", "currentPage"],
+                apply: (ctx) => {
+                    // Constants
+                    const title = ctx.currentPage.wikiTitle;
+                    const revisionId = +ctx.currentPage.params.get("oldid") || undefined;
+                    /** @type {HTMLElement} */
+                    let editButton;
+                    // Clean up on dispose
+                    ctx.on("dispose", () => {
+                        if (editButton) {
+                            editButton.remove();
+                        }
                     });
-                    $(".page-tool-link#ca-inpageedit").insertAfter($(".page-tool-link#ca-edit"));
-                    break;
-                }
-            }
+
+                    // Utils
+                    const QuickEditSvg = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-pencil-bolt ipe-icon"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4"></path><path d="M13.5 6.5l4 4"></path><path d="M19 16l-2 3h4l-2 3"></path></svg>';
+                    /** @type {(ev: Event) => void} */
+                    const openEditor = (ev) => {
+                        ev.preventDefault();
+                        ctx.quickEdit.showModal({
+                            title: title.toString(),
+                            revision: revisionId,
+                        });
+                    };
+
+                    // This page is not editable
+                    if (title.getNamespaceId() < 0 || !ctx.wiki.hasRight("edit")) {
+                        ctx.scope.dispose();
+                        return;
+                    }
+
+                    const isVector2022 = document.body.classList.contains("skin-vector-2022");
+                    const isMoeSkin = document.body.classList.contains("skin-moeskin");
+                    if (isVector2022) {
+                        const caEdit = document.getElementById("ca-edit");
+                        if (caEdit) {
+                            editButton = document.createElement("li");
+                            editButton.id = "ca-quick-edit";
+                            editButton.className = "vector-tab-noicon mw-list-item";
+
+                            const link = document.createElement("a");
+                            link.href = "#";
+                            link.innerHTML = `编辑 ${QuickEditSvg}`;
+                            link.addEventListener("click", openEditor);
+
+                            editButton.appendChild(link);
+                            caEdit.insertAdjacentElement("beforebegin", editButton);
+                        }
+                    } else if (isMoeSkin) {
+                        /** @type {HTMLAnchorElement} */
+                        const caEdit = document.querySelector(".page-tool-link#ca-edit");
+                        if (caEdit) {
+                            // @ts-ignore caEdit is HTMLAnchorElement
+                            editButton = caEdit.cloneNode(true);
+                            editButton.id = "ca-inpageedit";
+                            editButton.setAttribute("href", "#");
+                            editButton.addEventListener("click", openEditor);
+                            editButton.querySelector(".moe-icon").innerHTML = QuickEditSvg.replace("ipe-icon", "");
+                            editButton.querySelector(".tooltip").textContent = "使用 IPE 编辑";
+
+                            caEdit.insertAdjacentElement("afterend", editButton);
+                        }
+                    }
+                },
+            });
         });
     },
 ]);
