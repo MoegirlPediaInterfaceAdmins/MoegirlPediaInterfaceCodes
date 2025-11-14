33d6b1e8d193794a754dbe8129f875ab4cfce68f
亚衣琴 <Saoutax@gmail.com>
2025-11-14 20:33:28 +0800

    feat(Gadget/inpageedit-next): add quick edit button

--- src/gadgets/inpageedit-next/Gadget-inpageedit-next.js
+++ src/gadgets/inpageedit-next/Gadget-inpageedit-next.js
@@ -19,5 +19,48 @@ window.RLQ.push([
                 type: "module",
             }),
         );
+
+        mw.hook("InPageEdit.ready").add((ctx) => {
+            const wgPageName = mw.config.get("wgRelevantPageName");
+            const wgRevisionId = mw.config.get("wgRevisionId");
+            switch (mw.config.get("skin")) {
+                case "vector-2022": {
+                    $("#ca-edit").after(
+                        $("<li>", {
+                            id: "ca-quick-edit",
+                            "class": "vector-tab-noicon mw-list-item",
+                        }).append(
+                            $("<a>", {
+                                href: "javascript:void(0)",
+                                text: typeof Wikiplus !== "undefined" ? "快速编辑(IPE)" : "快速编辑",
+                            }).on("click", () => {
+                                ctx.quickEdit({
+                                    page: wgPageName,
+                                    revision: wgRevisionId || undefined,
+                                });
+                            }),
+                        ),
+                    );
+                    break;
+                }
+                case "moeskin":
+                default: {
+                    mw.hook("moeskin.pagetools").add(({ addPageToolsButton }) => {
+                        const $btn = addPageToolsButton('<span style="align-self:center;font:0.7em bold;">IPE</span>', "快速编辑");
+                        $btn.attr({
+                            id: "ca-inpageedit",
+                            href: "javascript:void(0)",
+                        }).on("click", () => {
+                            ctx.quickEdit({
+                                page: wgPageName,
+                                revision: wgRevisionId || undefined,
+                            });
+                        });
+                    });
+                    $(".page-tool-link#ca-inpageedit").insertAfter($(".page-tool-link#ca-edit"));
+                    break;
+                }
+            }
+        });
     },
 ]);
