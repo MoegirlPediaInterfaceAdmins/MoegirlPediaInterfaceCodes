840847aa1d7de8f55f10ab43cf92aaf93825c840
AnnAngela <naganjue@vip.qq.com>
2026-03-03 11:03:21 +0800

    feat: 添加编辑、移动和创建请求功能，优化保护信息展示

--- src/gadgets/pageInfo/Gadget-pageInfo.js
+++ src/gadgets/pageInfo/Gadget-pageInfo.js
@@ -1,6 +1,13 @@
 "use strict";
 (async () => {
     const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+    const prefixNumber = (num) => {
+        let result = `${num}`;
+        if (result.length === 1) {
+            result = `0${result}`;
+        }
+        return result;
+    };
 
     await $.ready;
 
@@ -29,7 +36,8 @@
         }
     }
 
-    const { wgRestrictionEdit, wgRestrictionMove, wgRestrictionCreate } = mw.config.get(["wgRestrictionEdit", "wgRestrictionMove", "wgRestrictionCreate"]);
+    const { wgRestrictionEdit, wgRestrictionMove, wgRestrictionCreate, wgPageName } = mw.config.get(["wgRestrictionEdit", "wgRestrictionMove", "wgRestrictionCreate", "wgPageName"]);
+    const { talkPage, basePageName } = wgGetPageNames();
     const protectLevels = new Set();
     if (Array.isArray(wgRestrictionEdit) && wgRestrictionEdit.length > 0) {
         wgRestrictionEdit.forEach((level) => protectLevels.add(`protect-level-${level}`));
@@ -46,15 +54,23 @@
     if (protectLevels.size > 0) {
         const api = new mw.Api();
         await api.loadMessagesIfMissing(Array.from(protectLevels));
+        const intestactionsPromise = api.post({
+            action: "query",
+            prop: "info",
+            titles: "Mainpage",
+            formatversion: "2",
+            intestactions: "edit|move|create",
+            intestactionsdetail: "boolean",
+        });
         const protectionInfo = [];
         if (Array.isArray(wgRestrictionEdit) && wgRestrictionEdit.length > 0) {
-            protectionInfo.push(`${mw.msg("edit")}：${wgRestrictionEdit.map((level) => mw.msg(`protect-level-${level}`)).join("、")}`);
+            protectionInfo.push(`${mw.msg("edit")}：${wgRestrictionEdit.map((level) => mw.msg(`protect-level-${level}`)).join("、")}<ul id="edit-protection-request" style="display: none;"></ul>`); // 临时隐藏编辑请求按钮
         }
         if (Array.isArray(wgRestrictionMove) && wgRestrictionMove.length > 0) {
-            protectionInfo.push(`${mw.msg("move")}：${wgRestrictionMove.map((level) => mw.msg(`protect-level-${level}`)).join("、")}`);
+            protectionInfo.push(`${mw.msg("move")}：${wgRestrictionMove.map((level) => mw.msg(`protect-level-${level}`)).join("、")}<ul id="move-protection-request"></ul>`);
         }
         if (Array.isArray(wgRestrictionCreate) && wgRestrictionCreate.length > 0) {
-            protectionInfo.push(`${mw.msg("create")}：${wgRestrictionCreate.map((level) => mw.msg(`protect-level-${level}`)).join("、")}`);
+            protectionInfo.push(`${mw.msg("create")}：${wgRestrictionCreate.map((level) => mw.msg(`protect-level-${level}`)).join("、")}<ul id="create-protection-request"></ul>`);
         }
         const $protectionInfoContainer = $("<a>", {
             "class": "page-info-protection annotation",
@@ -73,5 +89,41 @@
         });
         $protectionInfoText.appendTo($protectionInfoContainer);
         $protectionInfoContainer.trigger("mouseout");
+        const { query: { pages: [{ actions }] } } = await intestactionsPromise;
+        const now = new Date();
+        const requestTitleSuffix = ` - ${mw.config.get("wgUserName")} - ${now.getFullYear()}.${prefixNumber(now.getMonth() + 1)}.${prefixNumber(now.getDate())}`;
+        if (actions.edit === false && talkPage) {
+            $("<a/>").attr("href", "javascript:void(0);").text("提出编辑请求").on("click", () => {
+                const editRequestURL = new URL(`${mw.config.get("wgServer")}${mw.config.get("wgScriptPath")}/index.php`);
+                editRequestURL.searchParams.set("action", "edit");
+                editRequestURL.searchParams.set("preload", `Template:编辑请求/${basePageName !== false && /^MediaWiki:Conversiontable\/zh-[a-z]+$/.test(wgPageName) ? basePageName : "comment"}`);
+                editRequestURL.searchParams.set("preloadtitle", `编辑请求${requestTitleSuffix}`);
+                editRequestURL.searchParams.set("section", "new");
+                editRequestURL.searchParams.set("title", talkPage);
+                window.open(editRequestURL, "_blank");
+            }).appendTo($("<li/>").appendTo($protectionInfoContainer.find("#edit-protection-request")));
+        }
+        if (actions.move === false) {
+            $("<a/>").attr("href", "javascript:void(0);").text("提出移动请求").on("click", () => {
+                const moveRequestURL = new URL(`${mw.config.get("wgServer")}${mw.config.get("wgScriptPath")}/index.php`);
+                moveRequestURL.searchParams.set("action", "edit");
+                moveRequestURL.searchParams.set("preload", "Template:移动请求");
+                moveRequestURL.searchParams.set("preloadtitle", `移动请求${requestTitleSuffix}`);
+                moveRequestURL.searchParams.set("section", "new");
+                moveRequestURL.searchParams.set("title", "萌娘百科讨论:讨论版/操作申请");
+                window.open(moveRequestURL, "_blank");
+            }).appendTo($("<li/>").appendTo($protectionInfoContainer.find("#move-protection-request")));
+        }
+        if (actions.create === false) {
+            $("<a/>").attr("href", "javascript:void(0);").text("提出创建请求").on("click", () => {
+                const createRequestURL = new URL(`${mw.config.get("wgServer")}${mw.config.get("wgScriptPath")}/index.php`);
+                createRequestURL.searchParams.set("action", "edit");
+                createRequestURL.searchParams.set("preload", "Template:创建请求");
+                createRequestURL.searchParams.set("preloadtitle", `创建请求${requestTitleSuffix}`);
+                createRequestURL.searchParams.set("section", "new");
+                createRequestURL.searchParams.set("title", "萌娘百科讨论:讨论版/操作申请");
+                window.open(createRequestURL, "_blank");
+            }).appendTo($("<li/>").appendTo($protectionInfoContainer.find("#create-protection-request")));
+        }
     }
 })();
