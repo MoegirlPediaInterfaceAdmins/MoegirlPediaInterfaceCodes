9d2c5981230f00b27f288d43fc11e6d1b001f8bd
AnnAngela <naganjue@vip.qq.com>
2026-03-06 14:12:31 +0800

    fix: 优化 scheduler 的 yieldViaMessageChannel 实现方式

--- scripts/generatePolyfill/customPolyfill/library/scheduler/raw.js
+++ scripts/generatePolyfill/customPolyfill/library/scheduler/raw.js
@@ -1,11 +1,9 @@
 if (!Reflect.has(globalThis, "scheduler")) {
-    const yieldViaMessageChannel = (() => {
-        const channel = new MessageChannel();
-        return () => new Promise((resolve) => {
-            channel.port1.onmessage = resolve;
-            channel.port2.postMessage(null);
-        });
-    })();
+    const channel = new MessageChannel();
+    const yieldViaMessageChannel = () => new Promise((resolve) => {
+        channel.port1.onmessage = resolve;
+        channel.port2.postMessage(null);
+    });
     globalThis.scheduler = {
         yield: yieldViaMessageChannel,
         postTask: (callback, { delay = 0 } = {}) => setTimeout(callback, delay),
