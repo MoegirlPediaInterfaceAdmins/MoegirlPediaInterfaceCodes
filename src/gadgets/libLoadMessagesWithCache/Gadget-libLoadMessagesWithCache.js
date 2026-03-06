abc978c608a4f4eb67b3e0a958b39c68047daf9f
AnnAngela <naganjue@vip.qq.com>
2026-03-06 23:42:57 +0800

    feat: 添加 libLoadMessagesWithCache 相关功能及配置

---
+++ src/gadgets/libLoadMessagesWithCache/Gadget-libLoadMessagesWithCache.js
@@ -0,0 +1,19 @@
+"use strict";
+(() => {
+    const localObjectStorage = new LocalObjectStorage("libLoadMessagesWithCache");
+    mw.messages.set(localObjectStorage.getItem("messagesCache", {}));
+    const api = new mw.Api();
+    window.libLoadMessagesWithCache = {
+        loadMessagesIfMissing: async (messages) => {
+            const result = await api.loadMessagesIfMissing(messages);
+            if (result === true) {
+                const messagesCache = localObjectStorage.getItem("messagesCache", {});
+                for (const message of messages) {
+                    messagesCache[message] = mw.msg(message);
+                }
+                localObjectStorage.setItem("messagesCache", messagesCache);
+            }
+            return result;
+        },
+    };
+})();
