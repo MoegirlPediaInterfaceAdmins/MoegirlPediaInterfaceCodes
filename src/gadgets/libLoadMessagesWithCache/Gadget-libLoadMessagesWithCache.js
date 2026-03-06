5260bbfa12a81847b9cb3bc6fe64a404e5999cef
AnnAngela <naganjue@vip.qq.com>
2026-03-06 23:48:36 +0800

    fix: 修复 LocalObjectStorage 键的使用以支持用户语言缓存

--- src/gadgets/libLoadMessagesWithCache/Gadget-libLoadMessagesWithCache.js
+++ src/gadgets/libLoadMessagesWithCache/Gadget-libLoadMessagesWithCache.js
@@ -1,17 +1,18 @@
 "use strict";
 (() => {
-    const localObjectStorage = new LocalObjectStorage(`libLoadMessagesWithCache:${mw.config.get("wgUserLanguage")}`);
-    mw.messages.set(localObjectStorage.getItem("messagesCache", {}));
+    const localObjectStorage = new LocalObjectStorage("libLoadMessagesWithCache");
+    const key = `messagesCache:${mw.config.get("wgUserLanguage")}`;
+    mw.messages.set(localObjectStorage.getItem(key, {}));
     const api = new mw.Api();
     window.libLoadMessagesWithCache = {
         loadMessagesIfMissing: async (messages) => {
             const result = await api.loadMessagesIfMissing(messages);
             if (result === true) {
-                const messagesCache = localObjectStorage.getItem("messagesCache", {});
+                const messagesCache = localObjectStorage.getItem(key, {});
                 for (const message of messages) {
                     messagesCache[message] = mw.msg(message);
                 }
-                localObjectStorage.setItem("messagesCache", messagesCache);
+                localObjectStorage.setItem(key, messagesCache);
             }
             return result;
         },
