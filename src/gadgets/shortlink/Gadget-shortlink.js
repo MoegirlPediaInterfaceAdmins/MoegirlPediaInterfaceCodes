1a324e9a2b1bbd04d2eee3b2ca4e203e1afa9d1f
Saki Itomi <gpc2843661009@gmail.com>
2026-03-23 23:32:00 +0800

    feat(Gadget/shortlink): 🚸 指定 MoeSkin 下自定义图标 (#873)
    Co-authored-by: AnnAngela <naganjue@vip.qq.com>

--- src/gadgets/shortlink/Gadget-shortlink.js
+++ src/gadgets/shortlink/Gadget-shortlink.js
@@ -120,7 +120,8 @@ $(() => {
     switch (skin) {
         case "moeskin": {
             mw.hook("moeskin.pagetools").add(({ addPageToolsButton }) => {
-                const $btn = addPageToolsButton("<span style=\"align-self:center;\">短</span>", wgULS("短链接", "短網址"), "extra");
+                const iconHTML = '<svg class="tabler-icon tabler-icon-link-bolt" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.072 0a4.993 4.993 0 0 1 -.001 7.072" /><path d="M12.603 18.534a5.07 5.07 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /><path d="M19 16l-2 3h4l-2 3" /></svg>';
+                const $btn = addPageToolsButton(iconHTML, wgULS("短链接", "短網址"), "extra");
                 $btn.attr({
                     id: "ca-shortlink",
                     href: "#",
