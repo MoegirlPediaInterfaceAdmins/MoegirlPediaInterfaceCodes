68fe7adc79f321197f15696561253f4fefd55534
Saoutax <Saoutax@gmail.com>
2026-08-27 12:18:52 +0800

    fix(Gadget/legacyMainpage): 排除文章区

--- src/gadgets/legacyMainpage/Gadget-legacyMainpage.js
+++ src/gadgets/legacyMainpage/Gadget-legacyMainpage.js
@@ -1,6 +1,6 @@
 "use strict";
 $(() => {
-    if (mw.config.get("wgPageName") === "Mainpage") {
+    if (mw.config.get("wgPageName") === "Mainpage" && !location.hash.startsWith('#/post') && !location.hash.startsWith('#/create')) {
         location.hash = "/legacy";
     }
 });
