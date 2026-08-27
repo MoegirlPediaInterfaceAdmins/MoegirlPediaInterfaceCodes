6b0af0a1ba56be6ad55cee0997dc68b818ed12aa
Saoutax <Saoutax@gmail.com>
2026-08-27 12:28:25 +0800

    style: lint

--- src/gadgets/legacyMainpage/Gadget-legacyMainpage.js
+++ src/gadgets/legacyMainpage/Gadget-legacyMainpage.js
@@ -1,6 +1,6 @@
 "use strict";
 $(() => {
-    if (mw.config.get("wgPageName") === "Mainpage" && !location.hash.startsWith('#/post') && !location.hash.startsWith('#/create')) {
+    if (mw.config.get("wgPageName") === "Mainpage" && !location.hash.startsWith("#/post") && !location.hash.startsWith("#/create")) {
         location.hash = "/legacy";
     }
 });
